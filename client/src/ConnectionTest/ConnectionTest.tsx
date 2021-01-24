759baf534a9f730ac284b081cb5e23148cc6a26b
Ethan <ehinchli@sfu.ca>
2021-01-23 17:58:09 -0800

    Add Prettier to frontend

--- client/src/ConnectionTest/ConnectionTest.tsx
+++ client/src/ConnectionTest/ConnectionTest.tsx
@@ -1,58 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import Alert from 'react-bootstrap/esm/Alert';
-import { API_EXAMPLE } from '../endpoints';
+import React, { useEffect, useState } from "react";
+import Alert from "react-bootstrap/esm/Alert";
+import { API_EXAMPLE } from "../endpoints";
 
 enum reqState {
-  LOADING,
-  SUCCESS,
-  ERROR
+    LOADING,
+    SUCCESS,
+    ERROR,
 }
 
 const reqAlertClass = {
-  [reqState.LOADING]: 'primary',
-  [reqState.SUCCESS]: 'success',
-  [reqState.ERROR]: 'danger',
-}
+    [reqState.LOADING]: "primary",
+    [reqState.SUCCESS]: "success",
+    [reqState.ERROR]: "danger",
+};
 
 const reqMessage = {
-  [reqState.LOADING]: "Trying to connect to the API...",
-  [reqState.SUCCESS]: <><i className="fa fa-check"></i> Client successfully connected to the CBR Manager API.</>,
-  [reqState.ERROR]: <><i className="fa fa-exclamation-triangle"></i> Failed to connect to the CBR Manager API.</>,
-}
+    [reqState.LOADING]: "Trying to connect to the API...",
+    [reqState.SUCCESS]: (
+        <>
+            <i className="fa fa-check"></i> Client successfully connected to the CBR Manager API.
+        </>
+    ),
+    [reqState.ERROR]: (
+        <>
+            <i className="fa fa-exclamation-triangle"></i> Failed to connect to the CBR Manager API.
+        </>
+    ),
+};
 
 const ConnectionTest = () => {
-  const [reqStatus, setReqStatus] = useState(reqState.LOADING);
+    const [reqStatus, setReqStatus] = useState(reqState.LOADING);
 
-  useEffect(() => {
-    const testAPIConnection = async () => {
-      try {
-        const resp = await fetch(API_EXAMPLE);
+    useEffect(() => {
+        const testAPIConnection = async () => {
+            try {
+                const resp = await fetch(API_EXAMPLE);
 
-        if(!resp.ok) {
-          throw new Error("Response not ok")
-        }
+                if (!resp.ok) {
+                    throw new Error("Response not ok");
+                }
 
-        setReqStatus(reqState.SUCCESS);
-      }
-      catch(e) {
-        setReqStatus(reqState.ERROR);
-      }
-    }
+                setReqStatus(reqState.SUCCESS);
+            } catch (e) {
+                setReqStatus(reqState.ERROR);
+            }
+        };
 
-    testAPIConnection();
-  }, []);
+        testAPIConnection();
+    }, []);
 
-  return (
-    <div>
-      <h1>CBR Manager</h1>
-      <Alert variant="success">
-        <i className="fa fa-check"></i> You're successfully running the CBR Manager client.
-      </Alert>
-      <Alert variant={reqAlertClass[reqStatus]}>
-        {reqMessage[reqStatus]}
-      </Alert>
-    </div>
-  );
-}
+    return (
+        <div>
+            <h1>CBR Manager</h1>
+            <Alert variant="success">
+                <i className="fa fa-check"></i> You're successfully running the CBR Manager client.
+            </Alert>
+            <Alert variant={reqAlertClass[reqStatus]}>{reqMessage[reqStatus]}</Alert>
+        </div>
+    );
+};
 
 export default ConnectionTest;
--- client/src/endpoints.ts
+++ client/src/endpoints.ts
@@ -1,4 +1,7 @@
-const BASE_URL = process.env.NODE_ENV === 'development' ? `http://${window.location.hostname}:8000/api/` : '/api/';
-const FORMAT_JSON = '?format=json';
+const BASE_URL =
+    process.env.NODE_ENV === "development"
+        ? `http://${window.location.hostname}:8000/api/`
+        : "/api/";
+const FORMAT_JSON = "?format=json";
 
-export const API_EXAMPLE = BASE_URL + 'example' + FORMAT_JSON;
+export const API_EXAMPLE = BASE_URL + "example" + FORMAT_JSON;

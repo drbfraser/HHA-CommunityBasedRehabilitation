09e063a515ec497433e1d4278a1af17d6dbc7a31
gwolf <griffin_wolf@sfu.ca>
2021-01-30 15:38:38 -0800

    [ckl47, gwolf, pla90] Add user and visit database models
    Created User, Visit, and Improvement tables, and changed Client table. Added comments to explain why some columns are optional, and when some columns will be updated. Included the Pillow library for the ImageField() column type. Trio programmed by Argus, Roger, Griffin.

--- server/cbr_api/models.py
+++ server/cbr_api/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
-
 class Zone(models.Model):
     zone_name = models.CharField(max_length=200)
 
@@ -12,17 +10,85 @@ class Disability(models.Model):
 class Client(models.Model):
     client_id = models.PositiveIntegerField(primary_key=True)
     public_id = models.PositiveIntegerField(unique=True)
-    longitude = models.DecimalField(max_digits=22, decimal_places=16)
-    latitude = models.DecimalField(max_digits=22, decimal_places=16)
-    zone = models.ForeignKey(Zone, on_delete=models.PROTECT)
-    village_name = models.CharField(max_length=200)
     birth_date = models.BigIntegerField()
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     gender = models.BooleanField()
     register_date = models.BigIntegerField()
-    contact_number = models.CharField(max_length=200)
+    contact_number = models.CharField(max_length=200, blank=True) # if contact info available
+    
+    # updated whenever a visit form is submitted for this client
+    current_longitude = models.DecimalField(max_digits=22, decimal_places=16)
+    current_latitude = models.DecimalField(max_digits=22, decimal_places=16)
+    current_zone = models.ForeignKey(Zone, on_delete=models.PROTECT)
+    current_village = models.CharField(max_length=200)
+    current_picture = models.ImageField(upload_to='images/', blank=True) # if picture available
 
-class Junction(models.Model):
+class DisabilityJunction(models.Model):
     disability = models.ForeignKey(Disability, on_delete=models.CASCADE) 
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    
+class User(models.Model):
+    class UserType(models.TextChoices):
+        WORKER = 'WRK', _('Worker')
+        CLINICIAN = 'CLN', _('Clinician')
+        ADMIN = 'ADM', _('Admin')
+
+    user_id = models.PositiveIntegerField(primary_key=True)
+    user_type = models.CharField(max_length=3, choices=UserType.choices)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    zone = models.ForeignKey(Zone, on_delete=models.PROTECT)
+    phone_number = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200) # To be changed: Add password hashing
+    deactivated = models.BooleanField(default=False)
+
+class Visit(models.Model):
+    class Risk(models.TextChoices):
+        LOW = 'LO', _('Low')
+        MEDIUM = 'ME', _('Medium')
+        HIGH = 'HI', _('High')
+        CRITICAL = 'CR', _('Critical')
+        
+    class Purpose(models.TextChoices):
+        CBR = 'CBR', _('Community Based Rehabilitation')
+        REFERRAL = 'REF', _('Disability Centre Referral')
+        FOLLOWUP = 'FOL', _('Referral Follow-Up')
+        
+    class Goal(models.TextChoices):
+        CANCELLED = 'CAN', _('Cancelled')
+        ONGOING = 'GO', _('Ongoing')
+        CONCLUDED = 'CON', _('Concluded')
+        
+    visit_id = models.PositiveIntegerField(primary_key=True)
+    client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    caregiver_present = models.BooleanField(default=False)
+    caregiver_contact = models.CharField(max_length=200, blank=True) # if caregiver present
+    date_visited = models.BigIntegerField()
+    purpose = models.CharField(max_length=3, choices=Purpose.choices)
+    
+    # if purpose is CBR
+    health_risk = models.CharField(max_length=2, choices=Risk.choices, blank=True)
+    health_goal_met = models.CharField(max_length=3, choices=Goal.choices, blank=True)
+    health_goal_outcome = models.CharField(max_length=500, blank=True)
+    social_risk = models.CharField(max_length=2, choices=Risk.choices, blank=True)
+    social_goal_met = models.CharField(max_length=3, choices=Goal.choices, blank=True)
+    social_goal_outcome = models.CharField(max_length=500, blank=True)
+    education_risk = models.CharField(max_length=2, choices=Risk.choices, blank=True)
+    education_goal_met = models.CharField(max_length=3, choices=Goal.choices, blank=True)
+    education_goal_outcome = models.CharField(max_length=500, blank=True)
+    
+    # these values will be copied to the client
+    visit_longitude = models.DecimalField(max_digits=22, decimal_places=16)
+    visit_latitude = models.DecimalField(max_digits=22, decimal_places=16)
+    visit_zone = models.ForeignKey(Zone, on_delete=models.PROTECT)
+    visit_village = models.CharField(max_length=200)
+    visit_picture = models.ImageField(upload_to='images/', blank=True) # if picture available
+    
+class Improvement(models.Model):
+    improvement_type = models.CharField(max_length=200)
+    improvement_desc = models.CharField(max_length=500)
+    visit = models.ForeignKey(Visit, on_delete=models.CASCADE)
+    

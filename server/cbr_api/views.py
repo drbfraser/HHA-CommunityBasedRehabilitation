4e59a2d57412d874cd856da80f247989c41ae52c
ArgusLi <ckl47@sfu.ca>
2021-02-07 14:14:37 -0800

    add user authentication
    Added jwt authentication to all generic client lists created by Griffin. Users must first login to retrieve access and refresh tokens. Access tokens expire in 5 minutes while refresh tokens expire in 15 days (using the refresh token to acquire a new access token leads to a new refresh token being generated that should still expire within the same period as original refresh token). The only requests that don't need authentications are logging in and creating a user.

--- server/cbr_api/views.py
+++ server/cbr_api/views.py
@@ -1,6 +1,32 @@
 from django.http import JsonResponse
+from django.views.generic.list import ListView
+from rest_framework.permissions import IsAuthenticated
+from cbr_api import models
+from cbr_api import serializers
+from rest_framework import generics
+from django.contrib.auth.models import User
 
 
-def example(request):
-    if request.method == "GET":
-        return JsonResponse({"status": "ok"})
+class UserCreate(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+class ClientList(generics.ListCreateAPIView):
+    queryset = models.Client.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = serializers.ClientSerializer
+
+class ClientDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Client.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = serializers.ClientSerializer
+
+class ZoneList(generics.ListCreateAPIView):
+    queryset = models.Zone.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = serializers.ZoneSerializer
+
+class ZoneDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Zone.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = serializers.ZoneSerializer

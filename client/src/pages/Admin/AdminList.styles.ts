cd43277a2ae88454957c14f4c8fbfd19eb226fb2
elac <eric_lac@sfu.ca>
2021-02-09 17:26:33 -0800

    Merge from master

--- client/src/pages/Admin/AdminList.styles.ts
+++ client/src/pages/Admin/AdminList.styles.ts
@@ -27,8 +27,4 @@ export const useStyles = makeStyles({
         display: "flex",
         float: "right",
     },
-    search: {
-        float: "right",
-        display: "inline-block",
-    },
 });

105df7519a0496a769ff886b872705edc6702a77
Ethan <ehinchli@sfu.ca>
2021-02-11 01:38:47 -0800

    Basic frontend auth code

--- client/src/util/endpoints.ts
+++ client/src/util/endpoints.ts
@@ -1,6 +1,40 @@
-const BASE_URL =
+import { getAuthToken } from "./auth";
+
+export const API_URL =
     process.env.NODE_ENV === "development"
         ? `http://${window.location.hostname}:8000/api/`
         : "/api/";
 
-export const API_EXAMPLE = BASE_URL + "example";
+export enum Endpoint {
+    LOGIN = "login/",
+    LOGIN_REFRESH = "login/refresh/",
+    CLIENTS = "clients",
+}
+
+export const apiFetch = async (
+    endpoint: Endpoint,
+    urlParams: string = "",
+    customInit: RequestInit = {}
+): Promise<Response> => {
+    const url = API_URL + endpoint + urlParams;
+    const authToken = await getAuthToken();
+
+    const init = {
+        ...customInit,
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer: ${authToken}`,
+            ...customInit.headers,
+        },
+    };
+
+    return fetch(url, init).then(async (resp) => {
+        if (!resp.ok) {
+            const msg = "API Fetch failed with HTTP Status " + resp.status;
+            console.error(msg);
+            return Promise.reject(msg);
+        }
+
+        return resp;
+    });
+};

3193421bc95929404378cce3368931cb3dbbbb91
roger <pla90@sfu.ca>
2021-02-11 18:23:52 -0800

    Update client models

--- server/cbr/settings.py
+++ server/cbr/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_filters",
     "drf_spectacular",
     "corsheaders",
     "cbr_api.apps.CbrApiConfig",

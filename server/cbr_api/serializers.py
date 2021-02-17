465e7970f18c173e89c791f1a04905e06f31eef4
ArgusLi <ckl47@sfu.ca>
2021-02-16 21:45:53 -0800

    Ran black and fixed the password update

--- server/cbr_api/serializers.py
+++ server/cbr_api/serializers.py
@@ -18,6 +18,22 @@ class UserCBRSerializer(serializers.ModelSerializer):
             "zone": {"required": True},
         }
 
+    def create(self, validated_data):
+        user = super().create(validated_data)
+
+        user.set_password(validated_data["password"])
+        user.save()
+
+        return user
+
+    def update(self, instance, validated_data):
+        user = super().update(instance, validated_data)
+
+        user.set_password(validated_data["password"])
+        user.save()
+
+        return user
+
 
 class ZoneSerializer(serializers.ModelSerializer):
     class Meta:

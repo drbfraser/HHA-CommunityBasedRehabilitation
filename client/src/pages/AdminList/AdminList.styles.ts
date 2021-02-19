96322d73d1667faaaac9e10c363a7ada5ac079d5
a-arshad <aaa117@sfu.ca>
2021-02-19 02:03:44 -0800

    Removed outer card margins on mobile, Added column toggle on datagrid

--- client/src/pages/AdminList/AdminList.styles.ts
+++ client/src/pages/AdminList/AdminList.styles.ts
@@ -1,11 +1,11 @@
 import { makeStyles } from "@material-ui/core/styles";
-import { mediaCompressedDataGrid } from "styles/DataGrid.styles";
+import { mediaMobile } from "theme.styles";
 
 export const useStyles = makeStyles({
     container: {
         height: "calc(100vh - 175px)",
         minHeight: 400,
-        padding: "5px 0px 50px 0px",
+        padding: "5px 0px 25px 0px",
     },
     icon: {
         padding: "0px 10px 0px 0px",
@@ -14,10 +14,24 @@ export const useStyles = makeStyles({
         float: "right",
         display: "flex",
     },
-    [mediaCompressedDataGrid]: {
+    dataGridWrapper: {
+        height: "100%",
+        width: "100%",
+        marginTop: "36px",
+    },
+    [mediaMobile]: {
+        container: {
+            height: "calc(100vh - 181px)",
+            paddingBottom: "40px",
+        },
         topContainer: {
             float: "none",
             justifyContent: "center",
+        },
+        dataGridWrapper: {
+            height: "100%",
+            width: "100%",
+            marginTop: 0,
         },
     },
 });

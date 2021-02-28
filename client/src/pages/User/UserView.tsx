d38d6e9ce24ca37dedd92cbc586202d6b46be8e9
henryye <yixuy@sfu.ca>
2021-02-27 19:17:58 -0800

    Delete the user edit page and reduce some padding in profile page

--- client/src/pages/User/UserView.tsx
+++ client/src/pages/User/UserView.tsx
@@ -1,25 +1,15 @@
-import React from "react";
-import EditIcon from "@material-ui/icons/Edit";
 import { useStyles } from "./styles";
 import { initialValues } from "./fields";
-import { useHistory } from "react-router-dom";
-import { Button } from "@material-ui/core";
 
 const UserView = (props: any) => {
     const styles = useStyles();
-    const history = useHistory();
-    const handleEdit = () => history.push("/user/edit");
+
     // Read From initialValues field further from Server
     return (
         <div className={styles.container}>
             <h1>
                 {initialValues.firstName} {initialValues.lastName}
             </h1>
-            <div className={styles.floatRight}>
-                <Button color="primary" onClick={handleEdit}>
-                    <EditIcon></EditIcon>Edit
-                </Button>
-            </div>
             <b>Username</b>
             <p>{initialValues.username}</p>
             <b>ID</b>

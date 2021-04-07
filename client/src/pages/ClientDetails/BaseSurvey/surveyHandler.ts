0a744a2fba4049e23aedd9ad9dc4cb0e8b5266b8
henryye <yixuy@sfu.ca>
2021-04-07 01:27:51 -0700

    Make the addSurvey as comment to use in the future

--- client/src/pages/ClientDetails/BaseSurvey/surveyHandler.ts
+++ client/src/pages/ClientDetails/BaseSurvey/surveyHandler.ts
@@ -1,13 +1,13 @@
 import { FormikHelpers } from "formik";
-import { apiFetch, Endpoint } from "util/endpoints";
+// import { apiFetch, Endpoint } from "util/endpoints";
 import { FormField, TFormValues } from "./surveyFormFields";
 import history from "util/history";
 
 const addSurvey = async (surveyInfo: string) => {
-    const init: RequestInit = {
-        method: "POST",
-        body: surveyInfo,
-    };
+    // const init: RequestInit = {
+    //     method: "POST",
+    //     body: surveyInfo,
+    // };
     // TODO: connect to the endpoint
     // return await apiFetch(Endpoint.SURVEY, "", init)
     //     .then((res) => {

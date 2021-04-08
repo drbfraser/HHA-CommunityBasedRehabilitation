ef209a84c0b24da0937a2a3d94ec2efa06bfbe50
ArgusLi <ckl47@sfu.ca>
2021-04-08 14:18:06 -0700

    Fix unnecessary escape characters in regex

--- client/src/pages/Admin/fields.ts
+++ client/src/pages/Admin/fields.ts
@@ -75,7 +75,7 @@ const passwordValidationShape = {
     [AdminField.password]: Yup.string()
         .label(fieldLabels[AdminField.password])
         .matches(
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
             "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
         )
         .required(),

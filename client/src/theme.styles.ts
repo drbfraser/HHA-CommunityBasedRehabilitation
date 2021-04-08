aa25a23d93ca21b9383e7b5a1ca8e9add599f2a3
Ethan <ehinchli@sfu.ca>
2021-04-07 21:30:07 -0700

    Basic stats charts for visits, referrals and disabilities

--- client/src/theme.styles.ts
+++ client/src/theme.styles.ts
@@ -5,12 +5,17 @@ export const mediaMobile = "@media (max-width: 800px)";
 export const themeColors = {
     blueBgLight: "#e3e8f4",
     blueBgDark: "#273364",
-    yellow: "rgb(250, 195, 90)",
-    yellowDark: "rgb(240, 170, 80)",
+    blueAccent: "#0083A4",
+    bluePale: "#6890AC",
+    yellow: "#FAC35A",
+    yellowDark: "#F0AA50",
     riskRed: "#ef5350",
     riskGreen: "#81C784",
     riskYellow: "#f0aa50",
     riskBlack: "#424242",
+    hhaBlue: "#009dc5",
+    hhaPurple: "#863b8f",
+    hhaGreen: "#56af31",
 };
 
 export const themeMui = createMuiTheme({
--- client/src/util/endpoints.ts
+++ client/src/util/endpoints.ts
@@ -19,6 +19,7 @@ export enum Endpoint {
     RISKS = "risks",
     DISABILITIES = "disabilities",
     VISIT = "visit/",
+    STATS = "stats",
 }
 
 export const APILoadError = "APILoadError";

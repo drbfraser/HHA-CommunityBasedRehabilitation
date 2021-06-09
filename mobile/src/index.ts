ced58e085666f88c4b2b2da38b5eb0ce16d8b25d
a-arshad <ali.stick.arshad@gmail.com>
2021-06-08 22:34:28 -0700

    Fixed navigation icons

--- mobile/src/index.ts
+++ mobile/src/index.ts
@@ -1,5 +1,5 @@
-import { AppRegistry } from "react-native";
+import { registerRootComponent } from "expo";
 
 import App from "./App";
 
-AppRegistry.registerComponent("main", () => App);
+registerRootComponent(App);

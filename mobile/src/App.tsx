19096400239617c61773ce1250d1670d186184f8
a-arshad <ali.stick.arshad@gmail.com>
2021-06-14 19:39:16 -0700

    added prettier

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -8,20 +8,20 @@ import theme from "./theme.styles";
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
-  return (
-    <Provider theme={theme}>
-      <NavigationContainer theme={theme}>
-        <Tab.Navigator>
-          {screensForUser(undefined).map((screen) => (
-            <Tab.Screen
-              key={screen.name}
-              name={screen.name}
-              component={screen.Component}
-              options={{ tabBarIcon: screen.iconName }}
-            />
-          ))}
-        </Tab.Navigator>
-      </NavigationContainer>
-    </Provider>
-  );
+    return (
+        <Provider theme={theme}>
+            <NavigationContainer theme={theme}>
+                <Tab.Navigator>
+                    {screensForUser(undefined).map((screen) => (
+                        <Tab.Screen
+                            key={screen.name}
+                            name={screen.name}
+                            component={screen.Component}
+                            options={{ tabBarIcon: screen.iconName }}
+                        />
+                    ))}
+                </Tab.Navigator>
+            </NavigationContainer>
+        </Provider>
+    );
 }
--- mobile/src/theme.styles.ts
+++ mobile/src/theme.styles.ts
@@ -4,14 +4,14 @@ import { Theme as PaperTheme } from "react-native-paper/lib/typescript/types";
 import { Theme as NavigationTheme } from "@react-navigation/native";
 
 const theme: PaperTheme & NavigationTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: themeColors.blueBgDark,
-    accent: themeColors.yellow,
-    border: DefaultTheme.colors.text,
-    card: DefaultTheme.colors.surface,
-  },
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: themeColors.blueBgDark,
+        accent: themeColors.yellow,
+        border: DefaultTheme.colors.text,
+        card: DefaultTheme.colors.surface,
+    },
 };
 
 export default theme;

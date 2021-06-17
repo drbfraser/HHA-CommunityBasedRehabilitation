a0334412a3c7437a8c1c27cbbb06b7716478ef21
Ming <zmgong@sfu.ca>
2021-06-16 23:00:26 -0700

    Now app show risk level icons correctly.

---
+++ mobile/src/util/risks.ts
@@ -0,0 +1,74 @@
+import { MaterialIcons  } from '@expo/vector-icons';
+import { themeColors } from "@cbr/common";
+
+export interface IRisk {
+    id: number;
+    client: number;
+    timestamp: number;
+    risk_type: RiskType;
+    risk_level: RiskLevel;
+    requirement: string;
+    goal: string;
+}
+
+export enum RiskType {
+    HEALTH = "HEALTH",
+    EDUCATION = "EDUCAT",
+    SOCIAL = "SOCIAL",
+}
+
+export interface IRiskType {
+    name: string;
+    Icon: string;
+}
+
+export const riskTypes: { [key: string]: IRiskType } = {
+    [RiskType.HEALTH]: {
+        name: "Health",
+        Icon: "local-hospital",
+    },
+    [RiskType.EDUCATION]: {
+        name: "Education",
+        Icon: "school",
+    },
+    [RiskType.SOCIAL]: {
+        name: "Social",
+        Icon: "record-voice-over",
+    },
+};
+
+export enum RiskLevel {
+    LOW = "LO",
+    MEDIUM = "ME",
+    HIGH = "HI",
+    CRITICAL = "CR",
+}
+
+export interface IRiskLevel {
+    name: string;
+    color: string;
+    level: number;
+}
+
+export const riskLevels: { [key: string]: IRiskLevel } = {
+    [RiskLevel.LOW]: {
+        level: 0,
+        name: "Low",
+        color: themeColors.riskGreen,
+    },
+    [RiskLevel.MEDIUM]: {
+        level: 1,
+        name: "Medium",
+        color: themeColors.riskYellow,
+    },
+    [RiskLevel.HIGH]: {
+        level: 4, // 1 high > 3 mediums, as specified by customer
+        name: "High",
+        color: themeColors.riskRed,
+    },
+    [RiskLevel.CRITICAL]: {
+        level: 13, // 1 critical > 3 highs, as specified by customer
+        name: "Critical",
+        color: themeColors.riskBlack,
+    },
+};

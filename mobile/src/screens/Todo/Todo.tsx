6e7ed55844a1ffdc62e9f0e4f2447ece59cfd080
Paul Ngo <paul_ngo@sfu.ca>
2021-06-16 23:44:49 -0700

    mobile: setup Todo so it checks & refreshes tokens

--- mobile/src/screens/Todo/Todo.tsx
+++ mobile/src/screens/Todo/Todo.tsx
@@ -1,17 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { View } from "react-native";
 import { Button, Text, Title } from "react-native-paper";
 import useStyles from "./Todo.styles";
 import { AuthContext } from "../../context/AuthContext";
+import { getAuthToken } from "@cbr/common";
 
 const Todo = () => {
     const styles = useStyles();
     const authContext = useContext(AuthContext);
+    const [isRefreshTokenValid, setRefreshTokenValid] = useState<boolean>();
+
+    useEffect(() => {
+        const checkRefreshToken = async () => {
+            // getAuthToken might make a call to /api/login/refresh
+            const authToken = await getAuthToken();
+            setRefreshTokenValid(authToken != null);
+        };
+        checkRefreshToken();
+    });
 
     return (
         <View style={styles.container}>
             <Title>This is a placeholder component screen.</Title>
             <Text>Due to be removed, once the app reaches completion!</Text>
+            {isRefreshTokenValid !== undefined ? (
+                <Text>Refresh and auth tokens valid? {`${isRefreshTokenValid}`}</Text>
+            ) : (
+                <></>
+            )}
             <Button mode="contained" onPress={authContext.logout}>
                 Logout
             </Button>

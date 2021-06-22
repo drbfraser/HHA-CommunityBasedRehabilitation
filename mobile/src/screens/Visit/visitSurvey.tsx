383d4b28c82b6007dbbfdf438ae94abc0c7fc05d
Jeffrey Wang <jtw14@sfu.ca>
2021-06-22 01:03:52 -0400

    import required modules from web

--- mobile/src/screens/Visit/visitSurvey.tsx
+++ mobile/src/screens/Visit/visitSurvey.tsx
@@ -1,100 +1,148 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { SafeAreaView, TextInput, View, Button, ScrollView } from "react-native";
 import useStyles from "./visitSurvey.style";
 import { Text, Title, List, Appbar, Checkbox } from "react-native-paper";
 import { ProgressSteps, ProgressStep } from "react-native-progress-steps";
+import { Field, FieldArray, Form, Formik, FormikHelpers, FormikProps } from "formik";
+import { Picker } from "@react-native-community/picker";
+import { useParams } from "react-router";
+import { useHistory } from "react-router-dom";
+import {
+    fieldLabels,
+    FormField,
+    ImprovementFormField,
+    OutcomeFormField,
+    initialValues,
+    provisionals,
+    initialValidationSchema,
+    visitTypeValidationSchema,
+    GoalStatus,
+} from "./formFields";
 
-class ExampleOne extends Component {
-    static navigationOptions = {
-        header: null,
+const NewVisit = () => {
+    const [activeStep, setActiveStep] = useState<number>(0);
+    const [enabledSteps, setEnabledSteps] = useState<FormField[]>([]);
+    // const [risks, setRisks] = useState<IRisk[]>([]);
+    const [submissionError, setSubmissionError] = useState(false);
+    const [loadingError, setLoadingError] = useState(false);
+    // const zones = useZones();
+    const { clientId } = useParams<{ clientId: string }>();
+
+    const isFinalStep = activeStep === enabledSteps.length && activeStep !== 0;
+
+    const nextStep = (values: any, helpers: FormikHelpers<any>) => {
+        if (isFinalStep) {
+            // form handler
+            // handleSubmit(values, helpers, setSubmissionError);
+            return;
+        } else {
+            if (activeStep === 0) {
+                helpers.setFieldValue(`${[FormField.client]}`, clientId);
+            }
+            setActiveStep(activeStep + 1);
+            helpers.setSubmitting(false);
+            helpers.setTouched({});
+        }
     };
 
-    defaultScrollViewProps = {
-        keyboardShouldPersistTaps: "handled",
-        contentContainerStyle: {
-            flex: 1,
-            justifyContent: "center",
-        },
+    const prevStep = () => {
+        setActiveStep(activeStep - 1);
     };
+};
 
-    onNextStep = () => {
-        console.log("called next step");
-    };
+export default NewVisit;
 
-    onPaymentStepComplete = () => {
-        alert("Visit Focus step completed!");
-    };
+// class ExampleOne extends Component {
+//     static navigationOptions = {
+//         header: null,
+//     };
 
-    onPrevStep = () => {
-        console.log("called previous step");
-    };
+//     defaultScrollViewProps = {
+//         keyboardShouldPersistTaps: "handled",
+//         contentContainerStyle: {
+//             flex: 1,
+//             justifyContent: "center",
+//         },
+//     };
 
-    onSubmitSteps = () => {
-        console.log("called on submit step.");
-    };
+//     onNextStep = () => {
+//         console.log("called next step");
+//     };
 
-    state = {
-        isValid: false,
-        errors: true,
-        removeBtnRow: true,
-        activeStep: 1,
-        disabledStepIconColor: "#2400A2",
-        isComplete: false,
-    };
+//     onPaymentStepComplete = () => {
+//         alert("Visit Focus step completed!");
+//     };
 
-    render() {
-        return (
-            <View style={{ flex: 1, marginTop: 50 }}>
-                <ProgressSteps
-                    activeStep={this.state.activeStep}
-                    disabledStepIconColor={this.state.disabledStepIconColor}
-                    isComplete={this.state.isComplete}
-                >
-                    <ProgressStep
-                        label="Visit Focus"
-                        onNext={this.onPaymentStepComplete}
-                        onPrevious={this.onPrevStep}
-                        scrollViewProps={this.defaultScrollViewProps}
-                    >
-                        <View style={{ alignItems: "center" }}>
-                            <Text>Visit Focus step content</Text>
-                        </View>
-                    </ProgressStep>
+//     onPrevStep = () => {
+//         console.log("called previous step");
+//     };
 
-                    <ProgressStep
-                        label="Health Visit"
-                        onNext={this.onNextStep}
-                        onPrevious={this.onPrevStep}
-                        scrollViewProps={this.defaultScrollViewProps}
-                    >
-                        <View style={{ alignItems: "center" }}>
-                            <Text>Health Visit step content</Text>
-                        </View>
-                    </ProgressStep>
-                    <ProgressStep
-                        label="Education Visit"
-                        onNext={this.onNextStep}
-                        onPrevious={this.onPrevStep}
-                        scrollViewProps={this.defaultScrollViewProps}
-                    >
-                        <View style={{ alignItems: "center" }}>
-                            <Text>Education Visit step content</Text>
-                        </View>
-                    </ProgressStep>
-                    <ProgressStep
-                        label="Social Visit"
-                        onPrevious={this.onPrevStep}
-                        onSubmit={this.onSubmitSteps}
-                        scrollViewProps={this.defaultScrollViewProps}
-                    >
-                        <View style={{ alignItems: "center" }}>
-                            <Text>Social Visit step content</Text>
-                        </View>
-                    </ProgressStep>
-                </ProgressSteps>
-            </View>
-        );
-    }
-}
+//     onSubmitSteps = () => {
+//         console.log("called on submit step.");
+//     };
 
-export default ExampleOne;
+//     state = {
+//         isValid: false,
+//         errors: true,
+//         removeBtnRow: true,
+//         activeStep: 1,
+//         disabledStepIconColor: "#2400A2",
+//         isComplete: false,
+//     };
+
+//     render() {
+//         return (
+//             <View style={{ flex: 1, marginTop: 50 }}>
+//                 <ProgressSteps
+//                     activeStep={this.state.activeStep}
+//                     disabledStepIconColor={this.state.disabledStepIconColor}
+//                     isComplete={this.state.isComplete}
+//                 >
+//                     <ProgressStep
+//                         label="Visit Focus"
+//                         onNext={this.onPaymentStepComplete}
+//                         onPrevious={this.onPrevStep}
+//                         scrollViewProps={this.defaultScrollViewProps}
+//                     >
+//                         <View style={{ alignItems: "center" }}>
+//                             <Text>Visit Focus step content</Text>
+//                         </View>
+//                     </ProgressStep>
+
+//                     <ProgressStep
+//                         label="Health Visit"
+//                         onNext={this.onNextStep}
+//                         onPrevious={this.onPrevStep}
+//                         scrollViewProps={this.defaultScrollViewProps}
+//                     >
+//                         <View style={{ alignItems: "center" }}>
+//                             <Text>Health Visit step content</Text>
+//                         </View>
+//                     </ProgressStep>
+//                     <ProgressStep
+//                         label="Education Visit"
+//                         onNext={this.onNextStep}
+//                         onPrevious={this.onPrevStep}
+//                         scrollViewProps={this.defaultScrollViewProps}
+//                     >
+//                         <View style={{ alignItems: "center" }}>
+//                             <Text>Education Visit step content</Text>
+//                         </View>
+//                     </ProgressStep>
+//                     <ProgressStep
+//                         label="Social Visit"
+//                         onPrevious={this.onPrevStep}
+//                         onSubmit={this.onSubmitSteps}
+//                         scrollViewProps={this.defaultScrollViewProps}
+//                     >
+//                         <View style={{ alignItems: "center" }}>
+//                             <Text>Social Visit step content</Text>
+//                         </View>
+//                     </ProgressStep>
+//                 </ProgressSteps>
+//             </View>
+//         );
+//     }
+// }
+
+// export default ExampleOne;

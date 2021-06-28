f2f7a583dce2f53fd93065bf2082a047c7d2a8e0
Paul Ngo <paul_ngo@sfu.ca>
2021-06-28 16:22:56 -0700

    mobile: add build types & derive API URL from .env (dev builds only)
    Based on https://docs.expo.io/guides/environment-variables/ and
    https://github.com/expo/fyi/blob/3d51e6b7ddd3dabc14f70fab8094db9d3984948b/config-flag-migration.md

---
+++ mobile/app.config.js
@@ -0,0 +1,65 @@
+import "dotenv/config";
+
+const commonAppConfig = {
+    slug: "cbr-mobile",
+    version: "0.0.1",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    splash: {
+        image: "./assets/splash.png",
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+    },
+    updates: {
+        fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+        supportsTablet: true,
+    },
+    android: {
+        adaptiveIcon: {
+            foregroundImage: "./assets/adaptive-icon.png",
+            backgroundColor: "#FFFFFF",
+        },
+        allowBackup: false,
+    },
+    web: {
+        favicon: "./assets/favicon.png",
+    },
+};
+
+module.exports = () => {
+    // TODO: When we add a script to build a production version of the app, ensure that the
+    //  command is prefixed like `APP_ENV=prod expo build:android`. We're not doing this yet
+    //  until we settle on what to do for app distribution.
+    const stagingApiUrl = "https://cbrs.cradleplatform.com/api/";
+
+    if (process.env.APP_ENV === "prod") {
+        return {
+            ...commonAppConfig,
+            name: "CBR",
+            extra: {
+                apiUrl: "https://cbrp.cradleplatform.com/api/",
+            },
+        };
+    } else if (process.env.APP_ENV === "staging") {
+        return {
+            ...commonAppConfig,
+            name: "CBR (Staging)",
+            extra: {
+                apiUrl: stagingApiUrl,
+            },
+        };
+    } else {
+        const devApiUrl = process.env.DEV_API_URL;
+        return {
+            ...commonAppConfig,
+            name: "CBR (Development)",
+            extra: {
+                // Fall back to the staging server if not set.
+                apiUrl: typeof devApiUrl === "string" ? devApiUrl : stagingApiUrl,
+            },
+        };
+    }
+};

2282fafcdd22fac419023c58376f5dd445badf35
Paul Ngo <paul_ngo@sfu.ca>
2021-07-06 12:57:09 -0700

    common: optimize empty Maps and getOtherDisabilityId
    * We make the maps for disabilities and zones read-only. There's no
      reason for an outside caller to ever need to modify any of these maps.
      As a result, we can use a pre-allocated empty map for error and
      loading values to prevent unneeded memory allocations. Empty maps are
      used as properties of objects stored in the global scope (i.e. the
      cached APIs for current user, disabilities, and zones), so they won't
      get garbage collected.
    * getOtherDisabilityId: Avoid creating a new Array containing a shallow
      copy of the disabilities and then trying to do a linear scan of that
      array. Costs extra heap memory and likely triples the number of
      operations for both the best- and worst-case scenarios (needs to
      allocate a new array, iterate through the map to shallow copy all
      entries of the map into the array, and then do a linear scan to
      search). Use an iterator to avoid all of this overhead.
      Should be helpful, as getOtherDisabilityId is often used in React
      components that rerender often.

--- common/src/util/hooks/disabilities.ts
+++ common/src/util/hooks/disabilities.ts
@@ -2,31 +2,36 @@
 //  database for proper persistence, not caching in TypeScript variables
 import { apiFetch, Endpoint } from "../endpoints";
 import { cachedAPIGet, cachedAPIHook, IAPICacheData } from "./cachedAPI";
+import emptyMap from "../internal/emptyMap";
 
 export interface IDisability {
     id: number;
     disability_type: string;
 }
 
-export type TDisabilityMap = Map<number, string>;
+export type TDisabilityMap = ReadonlyMap<number, string>;
 
 const cache: IAPICacheData<TDisabilityMap, TDisabilityMap, TDisabilityMap> = {
     doFetch: () => apiFetch(Endpoint.DISABILITIES),
     transformData: (ds: IDisability[]) => new Map(ds.map((d) => [d.id, d.disability_type])),
     promise: undefined,
     value: undefined,
-    loadingValue: new Map<number, string>(),
-    errorValue: new Map<number, string>(),
+    loadingValue: emptyMap,
+    errorValue: emptyMap,
 };
 
 const OTHER_DISABILITY_LABEL = "Other";
 
 export const getOtherDisabilityId = (disabilities: TDisabilityMap): number => {
-    return (
-        Array.from(disabilities).find(
-            ([, disabilityType]) => disabilityType === OTHER_DISABILITY_LABEL
-        )?.[0] ?? 0
-    );
+    const iterator = disabilities.entries();
+    let item: IteratorResult<[number, string]> = iterator.next();
+    while (!item.done) {
+        if (item.value[1] === OTHER_DISABILITY_LABEL) {
+            return item.value[0];
+        }
+        item = iterator.next();
+    }
+    return 0;
 };
 
 export const getDisabilities = async () => cachedAPIGet(cache);

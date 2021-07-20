c9514e50815de0c1a10531c29c30427d5ecdb3d1
jtw14 <jtw14@sfu.ca>
2021-07-19 21:53:20 -0400

    Refactor files
    Renames form handler and form fields and prepare to export them into common

---
+++ mobile/src/screens/NewVisit/visitFormHandler.ts
@@ -0,0 +1,54 @@
+import { ImprovementFormField, TFormValues } from "./visitFormFields";
+import { FormikHelpers } from "formik";
+import { FormField } from "./visitFormFields";
+import { apiFetch, Endpoint } from "@cbr/common";
+
+const addVisit = async (visitInfo: string) => {
+    const init: RequestInit = {
+        method: "POST",
+        body: visitInfo,
+    };
+
+    return await apiFetch(Endpoint.VISITS, "", init)
+        .then((res) => {
+            return res.json();
+        })
+        .then((res) => {
+            return res;
+        });
+};
+
+// TODO: implement latitude/longitude functionality (Added 0.0 for now as they are required fields in the database.)
+export const handleSubmit = async (
+    values: TFormValues,
+    helpers: FormikHelpers<TFormValues>,
+    setSubmissionError: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+    const newVisit = JSON.stringify({
+        client: values[FormField.client],
+        health_visit: values[FormField.health],
+        educat_visit: values[FormField.education],
+        social_visit: values[FormField.social],
+        zone: values[FormField.zone],
+        village: values[FormField.village],
+        longitude: 0.0,
+        latitude: 0.0,
+        improvements: Object.values(values[FormField.improvements])
+            .reduce((improvements, typedImprovement) => improvements.concat(typedImprovement))
+            .filter(
+                (improvement) =>
+                    improvement !== undefined && improvement[ImprovementFormField.enabled]
+            ),
+        outcomes: Object.values(values[FormField.outcomes]).filter(
+            (outcome) => outcome !== undefined
+        ),
+    });
+
+    try {
+        await addVisit(newVisit);
+        // history.goBack();
+    } catch (e) {
+        helpers.setSubmitting(false);
+        setSubmissionError(true);
+    }
+};

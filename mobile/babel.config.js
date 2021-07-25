5465a714726f2be9f34abf520bd1d865e239d5ba
jtw14 <jtw14@sfu.ca>
2021-07-25 17:56:40 -0400

    Resolve threads
    1. Delete unnecessary comments
    2. Delete unused file

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,14 +2,5 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: ["babel-preset-expo"],
-
-        // plugins: [
-        //     [
-        //         "module-resolver",
-        //         {
-        //             alias: path.resolve("node_modules/react"),
-        //         },
-        //     ],
-        // ],
     };
 };

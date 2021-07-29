9f70e2dfec7129650ffd7e7b8b0a916ba333c799
Paul Ngo <paul_ngo@sfu.ca>
2021-07-29 00:21:50 -0700

    set submit state on new client image blob failure
    Not doing this resulted in cases where getting the blob for the image
    failed, causing the form to never get out of the submitting state.

--- web/src/pages/ClientDetails/formHandler.ts
+++ web/src/pages/ClientDetails/formHandler.ts
@@ -23,34 +23,34 @@ export const handleSubmit = async (
     helpers: FormikHelpers<TFormValues>,
     setIsEditing: (isEditing: boolean) => void
 ) => {
-    const disabilities = await getDisabilities();
-    const updatedValues: Partial<IClient> = {
-        first_name: values.first_name,
-        last_name: values.last_name,
-        birth_date: timestampFromFormDate(values.birth_date as string),
-        gender: values.gender,
-        phone_number: values.phone_number,
-        zone: values.zone,
-        village: values.village,
-        caregiver_present: values.caregiver_present,
-        caregiver_name: values.caregiver_name,
-        caregiver_email: values.caregiver_email,
-        caregiver_phone: values.caregiver_phone,
-        longitude: values.longitude,
-        latitude: values.latitude,
-        disability: values.disability,
-        other_disability: values.disability.includes(getOtherDisabilityId(disabilities))
-            ? values.other_disability
-            : "",
-    };
-
-    const formData = objectToFormData(updatedValues);
-
-    if (values.pictureChanged && values.picture) {
-        await appendPicture(formData, values.picture, values.id);
-    }
-
     try {
+        const disabilities = await getDisabilities();
+        const updatedValues: Partial<IClient> = {
+            first_name: values.first_name,
+            last_name: values.last_name,
+            birth_date: timestampFromFormDate(values.birth_date as string),
+            gender: values.gender,
+            phone_number: values.phone_number,
+            zone: values.zone,
+            village: values.village,
+            caregiver_present: values.caregiver_present,
+            caregiver_name: values.caregiver_name,
+            caregiver_email: values.caregiver_email,
+            caregiver_phone: values.caregiver_phone,
+            longitude: values.longitude,
+            latitude: values.latitude,
+            disability: values.disability,
+            other_disability: values.disability.includes(getOtherDisabilityId(disabilities))
+                ? values.other_disability
+                : "",
+        };
+
+        const formData = objectToFormData(updatedValues);
+
+        if (values.pictureChanged && values.picture) {
+            await appendPicture(formData, values.picture, values.id);
+        }
+
         await updateClient(formData, values.id);
         setIsEditing(false);
     } catch (e) {

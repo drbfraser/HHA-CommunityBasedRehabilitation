976d6c1db58beaf52e5fa2f7f296d4e08e0ace51
Paul Ngo <paul_ngo@sfu.ca>
2021-07-29 00:21:50 -0700

    set submit state on new client image blob failure
    Not doing this resulted in cases where getting the blob for the image
    failed, causing the form to never get out of the submitting state.

--- web/src/pages/NewClient/formHandler.ts
+++ web/src/pages/NewClient/formHandler.ts
@@ -7,7 +7,7 @@ import {
     apiFetch,
     objectToFormData,
     APIFetchFailError,
-} from "@cbr/common//util/endpoints";
+} from "@cbr/common/util/endpoints";
 import history from "../../util/history";
 import { timestampFromFormDate } from "@cbr/common/util/dates";
 import { getDisabilities, getOtherDisabilityId } from "@cbr/common/util/hooks/disabilities";
@@ -30,52 +30,52 @@ const addClient = async (clientInfo: FormData) => {
 
 // TODO: implement latitude/longitude functionality (Added 0.0 for now as they are required fields in the database.)
 export const handleSubmit = async (values: TFormValues, helpers: FormikHelpers<TFormValues>) => {
-    const disabilities = await getDisabilities();
-
-    const newClient = {
-        birth_date: timestampFromFormDate(values.birthDate),
-        disability: values.disability,
-        other_disability: (values.disability as number[]).includes(
-            getOtherDisabilityId(disabilities)
-        )
-            ? values.otherDisability
-            : "",
-        first_name: values.firstName,
-        last_name: values.lastName,
-        gender: values.gender,
-        phone_number: values.phoneNumber,
-        longitude: 0.0,
-        latitude: 0.0,
-        zone: values.zone,
-        village: values.village,
-        caregiver_present: values.caregiverPresent,
-        caregiver_name: values.caregiverName,
-        caregiver_phone: values.caregiverPhone,
-        caregiver_email: values.caregiverEmail,
-        health_risk: {
-            risk_level: values.healthRisk,
-            requirement: values.healthRequirements,
-            goal: values.healthGoals,
-        },
-        social_risk: {
-            risk_level: values.socialRisk,
-            requirement: values.socialRequirements,
-            goal: values.socialGoals,
-        },
-        educat_risk: {
-            risk_level: values.educationRisk,
-            requirement: values.educationRequirements,
-            goal: values.educationGoals,
-        },
-    };
-
-    const formData = objectToFormData(newClient);
-
-    if (values.pictureChanged && values.picture) {
-        await appendPicture(formData, values.picture, null);
-    }
-
     try {
+        const disabilities = await getDisabilities();
+
+        const newClient = {
+            birth_date: timestampFromFormDate(values.birthDate),
+            disability: values.disability,
+            other_disability: (values.disability as number[]).includes(
+                getOtherDisabilityId(disabilities)
+            )
+                ? values.otherDisability
+                : "",
+            first_name: values.firstName,
+            last_name: values.lastName,
+            gender: values.gender,
+            phone_number: values.phoneNumber,
+            longitude: 0.0,
+            latitude: 0.0,
+            zone: values.zone,
+            village: values.village,
+            caregiver_present: values.caregiverPresent,
+            caregiver_name: values.caregiverName,
+            caregiver_phone: values.caregiverPhone,
+            caregiver_email: values.caregiverEmail,
+            health_risk: {
+                risk_level: values.healthRisk,
+                requirement: values.healthRequirements,
+                goal: values.healthGoals,
+            },
+            social_risk: {
+                risk_level: values.socialRisk,
+                requirement: values.socialRequirements,
+                goal: values.socialGoals,
+            },
+            educat_risk: {
+                risk_level: values.educationRisk,
+                requirement: values.educationRequirements,
+                goal: values.educationGoals,
+            },
+        };
+
+        const formData = objectToFormData(newClient);
+
+        if (values.pictureChanged && values.picture) {
+            await appendPicture(formData, values.picture, null);
+        }
+
         const client = await addClient(formData);
         history.push(`/client/${client.id}`);
     } catch (e) {
@@ -83,6 +83,7 @@ export const handleSubmit = async (values: TFormValues, helpers: FormikHelpers<T
         const detailedError =
             e instanceof APIFetchFailError ? e.buildFormError(fieldLabels) : `${e}`;
         alert(initialMessage + "\n" + detailedError);
+    } finally {
         helpers.setSubmitting(false);
     }
 };

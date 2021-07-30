f9db5390a0e9aec754e026cc99db1a652a5cd707
Ming <zmgong@sfu.ca>
2021-07-30 12:17:49 -0700

    Merged with current main branch to solve merge conflict.

--- common/index.ts
+++ common/index.ts
@@ -32,3 +32,5 @@ export * from "./src/util/visits";
 export * from "./src/util/searchOptions";
 export * from "./src/forms/newVisit/visitFormFields";
 export * from "./src/forms/newVisit/visitFormHandler";
+export * from "./src/forms/newVisit/visitFormFields";
+export * from "./src/forms/newVisit/visitFormHandler";

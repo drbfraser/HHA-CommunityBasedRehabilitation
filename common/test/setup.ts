c5a73623ddc7debb747891725f0272c4f0e8d3e2
henryye <yeyixu2018@gmail.com>
2021-08-02 18:11:02 -0700

    Fix the conflict

--- common/test/setup.ts
+++ common/test/setup.ts
@@ -3,15 +3,12 @@ import * as TestCommonConfig from "./testHelpers/testCommonConfiguration";
 import AbortController from "node-abort-controller";
 import { Request } from "node-fetch";
 
-beforeAll(() => {
-    // Mock FormData, as FormData isn't available in a nodejs environment.
-    // https://stackoverflow.com/a/59726560
-    // @ts-ignore
-    global.FormData = () => {};
-    global.Request = Request;
-});
+global.AbortController = AbortController;
 
-global.Request = Request;
+// Mock FormData, as FormData isn't available in a nodejs environment.
+// https://stackoverflow.com/a/59726560
+// @ts-ignore
+global.FormData = () => {};
 
 global.Request = Request;
 

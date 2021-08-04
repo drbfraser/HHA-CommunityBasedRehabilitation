d6b13585d240faf49c16f6428b2b152aadb229a0
Paul Ngo <paul_ngo@sfu.ca>
2021-08-03 19:23:51 -0700

    memoize FormikTextInput to improve form performance
    Formik apparently re-renders all components that use the FormikProps
    when it is set to validate on every change (true by default). This
    causes massive latency in Formik forms, especially when switching
    between multiple FormikTextInputs. Even on production builds with the
    standalone APK, the latency is noticeable.
    
    Fix this by memoizing the FormikTextInput so that the TextInput and
    HelperText are only re-rendered when the relevant properties change.
    This significantly increases performance on Formik forms using
    FormikTextInput.

--- mobile/src/components/FormikTextInput/FormikTextInput.tsx
+++ mobile/src/components/FormikTextInput/FormikTextInput.tsx
@@ -1,6 +1,6 @@
 import { FormikProps } from "formik";
-import React, { forwardRef } from "react";
-import { TextInput as NativeTextInput } from "react-native";
+import React, { forwardRef, memo } from "react";
+import { TextInput as NativeTextInput, TextInputProps as NativeTextInputProps } from "react-native";
 import { HelperText, TextInput } from "react-native-paper";
 import { shouldShowError } from "../../util/formikUtil";
 import { TextInputProps } from "react-native-paper/lib/typescript/components/TextInput/TextInput";
@@ -11,26 +11,53 @@ export type FormikTextInputProps<Field extends string> = Omit<TextInputProps, "t
     formikProps: FormikProps<any>;
 };
 
-const FormikTextInput = forwardRef<NativeTextInput, FormikTextInputProps<string>>((props, ref) => {
-    const { fieldLabels, field, formikProps, ...other } = props;
-    const isError = shouldShowError(formikProps, field);
+const BaseFormikTextInput = forwardRef<NativeTextInput, FormikTextInputProps<string>>(
+    (props, ref) => {
+        const { fieldLabels, field, formikProps, ...other } = props;
+        const isError = shouldShowError(formikProps, field);
+        return (
+            <>
+                <TextInput
+                    ref={ref}
+                    error={isError}
+                    label={props.fieldLabels[field]}
+                    value={formikProps.values[field]}
+                    onChangeText={formikProps.handleChange(field)}
+                    onEndEditing={() => formikProps.setFieldTouched(field)}
+                    disabled={formikProps.isSubmitting}
+                    mode="outlined"
+                    blurOnSubmit={false}
+                    {...other}
+                />
+                {isError ? <HelperText type="error">{formikProps.errors[field]}</HelperText> : null}
+            </>
+        );
+    }
+);
+
+const areFormikTextInputPropsEqual = (
+    oldProps: FormikTextInputProps<string>,
+    newProps: FormikTextInputProps<string>
+): boolean => {
+    if (oldProps.field !== newProps.field) {
+        return false;
+    }
+
+    const field = oldProps.field;
     return (
-        <>
-            <TextInput
-                ref={ref}
-                error={isError}
-                label={props.fieldLabels[field]}
-                value={formikProps.values[field]}
-                onChangeText={formikProps.handleChange(field)}
-                onEndEditing={() => formikProps.setFieldTouched(field)}
-                disabled={formikProps.isSubmitting}
-                mode="outlined"
-                blurOnSubmit={false}
-                {...other}
-            />
-            {isError ? <HelperText type="error">{formikProps.errors[field]}</HelperText> : null}
-        </>
+        oldProps.editable === newProps.editable &&
+        oldProps.key === newProps.key &&
+        oldProps.focusable === newProps.focusable &&
+        oldProps.disabled === newProps.disabled &&
+        oldProps.fieldLabels[field] === newProps.fieldLabels[field] &&
+        oldProps.formikProps.values[field] === newProps.formikProps.values[field] &&
+        shouldShowError(oldProps.formikProps, field) ===
+            shouldShowError(newProps.formikProps, field) &&
+        oldProps.formikProps.errors[field] === newProps.formikProps.errors[field] &&
+        oldProps.formikProps.isSubmitting === newProps.formikProps.isSubmitting
     );
-});
+};
+
+const FormikTextInput = memo(BaseFormikTextInput, areFormikTextInputPropsEqual);
 
 export default FormikTextInput;

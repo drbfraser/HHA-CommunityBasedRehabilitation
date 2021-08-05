f4daf66dee5bc0e2b77d77466b88009a939ab43f
jtw14 <jtw14@sfu.ca>
2021-08-05 18:38:30 -0400

    Implement VisitEntry and update common
    Adds Details function to VisitEntry. Removes material-ui's components with react native paper. Adds riskType to common. Icon may require update in future.

--- mobile/src/screens/ClientDetails/ClientTimeline/Entries/VisitEntry.tsx
+++ mobile/src/screens/ClientDetails/ClientTimeline/Entries/VisitEntry.tsx
@@ -12,6 +12,8 @@ import {
     IVisitSummary,
     outcomeGoalMets,
     useZones,
+    RiskType,
+    riskTypes,
 } from "@cbr/common";
 import {
     ActivityIndicator,
@@ -28,6 +30,8 @@ import useStyles from "./Entry.styles";
 import { ScrollView, View } from "react-native";
 import * as Yup from "yup";
 import { Formik, FormikProps } from "formik";
+import SkeletonPlaceholder from "react-native-skeleton-placeholder";
+import DataCard from "../../../../components/DataCard/DataCard";
 
 interface IEntryProps {
     visitSummary: IVisitSummary;
@@ -73,24 +77,71 @@ const VisitEntry = ({ visitSummary, refreshClient }: IEntryProps) => {
         </View>
     );
 
-    // const Summary = ({ clickable }: { clickable: boolean }) => {
-    //     const zone = zones.get(visitSummary.zone) ?? "Unknown";
+    const Details = () => {
+        if (!visit) {
+            return (
+                <SkeletonPlaceholder>
+                    <SkeletonPlaceholder.Item height={200} />
+                </SkeletonPlaceholder>
+            );
+        }
 
-    //     return (
-    //         <>
-    //             <b>Visit</b> in {zone} &nbsp;
-    //             {visitSummary.health_visit && (
-    //                 <RiskTypeChip risk={RiskType.HEALTH} clickable={clickable} />
-    //             )}{" "}
-    //             {visitSummary.educat_visit && (
-    //                 <RiskTypeChip risk={RiskType.EDUCATION} clickable={clickable} />
-    //             )}{" "}
-    //             {visitSummary.social_visit && (
-    //                 <RiskTypeChip risk={RiskType.SOCIAL} clickable={clickable} />
-    //             )}{" "}
-    //         </>
-    //     );
-    // };
+        const DetailAccordion = ({ type }: { type: RiskType }) => {
+            const improvements = visit.improvements
+                .filter((i) => i.risk_type === type)
+                .map((i) => ({
+                    title: i.provided,
+                    desc: i.desc,
+                }));
+
+            const outcomes = visit.outcomes
+                .filter((o) => o.risk_type === type)
+                .map((o) => ({
+                    title: outcomeGoalMets[o.goal_met].name,
+                    desc: o.outcome,
+                }));
+
+            if (!improvements.length && !outcomes.length) {
+                return <React.Fragment key={type} />;
+            }
+
+            let titleDescArr: string[] = [];
+
+            if (improvements.length) {
+                titleDescArr.push("Improvements");
+            }
+
+            if (outcomes.length) {
+                titleDescArr.push("Outcomes");
+            }
+
+            return (
+                <List.Accordion key={type} title={riskTypes[type].name}>
+                    {Boolean(improvements.length) && <DataCard data={improvements} />}
+                    {Boolean(outcomes.length) && <DataCard data={outcomes} />}
+                </List.Accordion>
+            );
+        };
+
+        return (
+            <>
+                <Card.Title title="New Visit Survey" />
+
+                <Card style={styles.createdCard}>
+                    <Card.Content>
+                        <Text>
+                            <Text style={styles.labelBold}>Visit Date:</Text>{" "}
+                            {timestampToDateTime(visit.date_visited)}
+                            <Text style={styles.labelBold}>Village:</Text> {visit.village}
+                        </Text>
+                    </Card.Content>
+                </Card>
+                {Object.values(RiskType).map((type) => (
+                    <DetailAccordion key={type} type={type} />
+                ))}
+            </>
+        );
+    };
 };
 
 export default VisitEntry;

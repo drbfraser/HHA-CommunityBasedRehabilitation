68a4875321a1637c3066b86d5b1f83fd0de7d5b3
zjzhao <zjzhao@sfu.ca>
2021-09-28 23:48:44 -0700

    Fix superuser & import Django models

--- server/cbr_api/admin.py
+++ server/cbr_api/admin.py
@@ -1,3 +1,20 @@
+from django.apps import apps
 from django.contrib import admin
 
 # Register your models here.
+from cbr_api.models import UserCBR
+
+# Customize interface below if needed
+@admin.register(UserCBR)
+class UserCBRManager(admin.ModelAdmin):
+    pass
+
+
+# Recommended to register models individually like UserCBRManager above if needed
+models = apps.get_models()
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        # Some models (ie. UserCBR) may already be registered
+        pass

e346d879a73b840ef748bf4d0360cab12f3020eb
nctin <nicholas_tin_2@sfu.ca>
2021-10-01 18:23:25 -0700

    additional comments

--- common/src/util/mobileImageSubmisson.ts
+++ common/src/util/mobileImageSubmisson.ts
@@ -2,6 +2,7 @@ export const appendMobilePict = async (formData: FormData, pictureUrl: string) =
     let localUri = pictureUrl;
     let filename = localUri.split("/").pop();
 
+    //find matching extension for file type
     let match = /\.(\w+)$/.exec(filename as string);
     let type = match ? `image/${match[1]}` : `image`;
 

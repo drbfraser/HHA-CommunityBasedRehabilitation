83153b34baa344578cc9e2f2d3848a8b26486802
zjzhao <zjzhao@sfu.ca>
2021-10-02 17:35:47 -0700

    add show all button to timeline ui

--- mobile/src/screens/ClientDetails/ClientTimeline/RecentActivity.tsx
+++ mobile/src/screens/ClientDetails/ClientTimeline/RecentActivity.tsx
@@ -1,8 +1,9 @@
 import { IVisitSummary } from "@cbr/common";
 import * as React from "react";
-import { View } from "react-native";
+import { useState } from "react";
+import { View, TouchableOpacity } from "react-native";
 import { TimelineDate } from "./TimelineDate";
-import Timeline, { IActivity } from "./Timeline";
+import Timeline, { IActivity, ActivityType } from "./Timeline";
 
 interface IActivityProps {
     clientVisits: IVisitSummary[];
@@ -12,17 +13,52 @@ interface IActivityProps {
 }
 
 export const RecentActivity = (props: IActivityProps) => {
+    const showMoreActivity = {
+        id: -1,
+        type: ActivityType.SHOWMORE,
+        date: -1,
+    };
+
+    const [showAllEntries, setShowAllEntries] = useState<boolean>(false);
+    const numEntriesToShow = 1;
+
+    const timelineItems = props.activity;
+    const topTimelineItems = timelineItems.slice(0, numEntriesToShow);
+
     return (
         <View>
-            {props.activity.map((presentActivity) => {
-                return (
+            {!showAllEntries
+                ? topTimelineItems.map((presentActivity) => {
+                      return (
+                          <Timeline
+                              key={presentActivity.id}
+                              activity={presentActivity}
+                              refreshClient={props.refreshClient}
+                          />
+                      );
+                  })
+                : timelineItems.map((presentActivity) => {
+                      return (
+                          <Timeline
+                              key={presentActivity.id}
+                              activity={presentActivity}
+                              refreshClient={props.refreshClient}
+                          />
+                      );
+                  })}
+            {!showAllEntries && timelineItems.length > numEntriesToShow && (
+                <TouchableOpacity
+                    onPress={() => {
+                        setShowAllEntries(true);
+                    }}
+                >
                     <Timeline
-                        key={presentActivity.id}
-                        activity={presentActivity}
-                        refreshClient={props.refreshClient}
+                        key={"showMore"}
+                        activity={showMoreActivity}
+                        refreshClient={() => {}}
                     />
-                );
-            })}
+                </TouchableOpacity>
+            )}
             <TimelineDate date={props.clientCreateDate} />
         </View>
     );

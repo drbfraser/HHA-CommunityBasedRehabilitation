2018074da790694a6db03ce4dc927ba80b7757ca
Mike Thai <mta24@sfu.ca>
2021-10-24 18:16:22 -0700

    Remove django socket test files

--- server/static/main.js
+++
@@ -1,6 +0,0 @@
-const socket = io("http://localhost:8000");
-
-socket.on("alert", (msg) => {
-  console.log("Status Resource: main.js received the alert", msg);
-  console.log("Status Resource: socket - ", socket);
-});

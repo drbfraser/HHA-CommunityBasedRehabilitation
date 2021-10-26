50b680b9388a34287d7599324fb3901a657da74d
Nick Tin <nicholas_tin_2@sfu.ca>
2021-10-26 04:51:23 +0000

    Revert "Merge branch '88-django-socketio-alert-prototype' into 'main'"
    This reverts merge request !84

--- server/cbr/wsgi.py
+++ server/cbr/wsgi.py
@@ -8,38 +8,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-import socketio
-import eventlet
-import eventlet.wsgi
 
 from django.core.wsgi import get_wsgi_application
 
-eventlet.monkey_patch(socket=True, select=True)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cbr.settings")
 
 application = get_wsgi_application()
-sio = socketio.Server(async_mode=None, cors_allowed_origins="*")
-application = socketio.WSGIApp(sio, application)
-
-
-@sio.on("connect")
-def connect(sid, data):
-    print("\n[SocketIO Server] User connected with socketID {}.\n".format(sid))
-    sio.emit("alert", {"sid": sid, "data": "[SocketIO Server] User Connected."})
-    pass
-
-
-@sio.on("disconnect")
-def disconnect(sid):
-    print("[SocketIO Server] User {} has disconnected.".format(sid))
-    pass
-
-
-@sio.on("newAlert")
-def newAlert(sid, data):
-    print("\n[SocketIO Server]: Received a new alert '{} from {}\n".format(data, sid))
-    sio.emit("pushAlert", {"data": "[SocketIO Server] pushAlert test message"})
-    pass
-
-
-eventlet.wsgi.server(eventlet.listen(("", 8000)), application).serve_forever()

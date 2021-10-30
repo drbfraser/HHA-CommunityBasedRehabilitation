be76a7ca3bb64ac244b38da2f97b8544a70f0f11
panwangp <richard_pan@sfu.ca>
2021-10-30 16:28:28 -0700

    The config entity module is created (the first level of decouple)

--- common/src/services/clientServices.ts
+++ common/src/services/clientServices.ts
@@ -1,12 +1,16 @@
 import { apiFetch, APIFetchFailError, Endpoint, objectToFormData } from "../util/endpoints";
+import { RequestConfigBuilder,ConfigEntity } from  "./tools/RequestConfigBuilder";
 
 const addClient = async (clientInfo: FormData) => {
-  const init: RequestInit = {
-      method: "POST",
-      body: clientInfo,
-  };
+  // const init: RequestInit = {
+  //     method: "POST",
+  //     body: clientInfo,
+  // };
 
-  return await apiFetch(Endpoint.CLIENTS, "", init)
+  const configEntity : ConfigEntity = RequestConfigBuilder.buildConfig("POST", clientInfo, Endpoint.CLIENTS,"");
+
+  // return await apiFetch(Endpoint.CLIENTS, "", init)
+  return await apiFetch(configEntity.endpoint, configEntity.userId, configEntity.init)
       .then((res) => {
           return res.json();
       })

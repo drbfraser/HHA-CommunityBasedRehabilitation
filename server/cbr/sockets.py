c806462439c0cf40042aff0af6daa6b8ae4bdf87
Mike Thai <mta24@sfu.ca>
2021-11-04 21:04:45 -0700

    Create property to track number of connections

--- server/cbr/sockets.py
+++ server/cbr/sockets.py
@@ -4,7 +4,6 @@ from cbr.settings import DEBUG, ALLOWED_HOSTS
 # The maximum allowable number of clients that can connect to the socketIO concurrently.
 # If this limit is reached, the next client that tries to connect will be disconnected.
 MAX_SOCKET_CONNECTION_LIMIT = 50
-CONNECTION_COUNT = 0 # A counter for the number of clients connected to the socketIO server
 
 cors_allowed_origins = "*" if DEBUG else ALLOWED_HOSTS
 sio = socketio.Server(
@@ -13,25 +12,25 @@ sio = socketio.Server(
     cors_allowed_origins=cors_allowed_origins,
 )
 
-# Set a property in the socket server to hold a maximum number of connections
+# Set a property in the socketIO server that represents the maximum allowable concurrent client connections
 sio.maxConnections = MAX_SOCKET_CONNECTION_LIMIT
+# Set a prooperty in the socketIO server to keep track of how many clients are currently connected
+sio.numCurrentConnections = 0
 
 @sio.on("connect")
 def connect(sid, environ):
-  global CONNECTION_COUNT
-  CONNECTION_COUNT = CONNECTION_COUNT + 1
+  sio.numCurrentConnections = sio.numCurrentConnections + 1
   sio.emit("alert", {"sid": sid, "data": "[SocketIO Server] User Connected."})
   print("[SocketIO Server] User connected with socketID {}.".format(sid))
 
-  if CONNECTION_COUNT > sio.maxConnections:
+  if sio.numCurrentConnections > sio.maxConnections:
     print("[SocketIO Server] Connection limit reached, disconnecting user with socketID {}.\n".format(sid))
     sio.disconnect(sid)
 
 
 @sio.on("disconnect")
 def disconnect(sid):
-    global CONNECTION_COUNT
-    CONNECTION_COUNT = CONNECTION_COUNT - 1
+    sio.numCurrentConnections = sio.numCurrentConnections - 1
     print("[SocketIO Server] User {} has disconnected.".format(sid))
 
 
@@ -39,7 +38,3 @@ def disconnect(sid):
 def newAlert(sid, data):
     print("[SocketIO Server]: Received a new alert '{} from {}".format(data, sid))
     sio.emit("pushAlert", {"data": "[SocketIO Server] pushAlert test message"})
-
-
-def main():
-  global CONNECTION_COUNT

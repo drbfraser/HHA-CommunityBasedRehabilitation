451ec875a3c3f4e59f49e1a7083ffab3398f4b31
nctin <nicholas_tin_2@sfu.ca>
2021-11-13 15:02:16 -0800

    fixed web stats filter by date

--- web/src/pages/Stats/StatsDateFilter.tsx
+++ web/src/pages/Stats/StatsDateFilter.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@material-ui/core";
 import { Field, Form, Formik } from "formik";
 import { TextField } from "formik-material-ui";
+import { timestampToFormDate } from "@cbr/common/util/dates";
 
 export interface IDateRange {
     from: string;
@@ -22,6 +23,7 @@ interface IProps {
 
 const StatsDateFilter = ({ open, onClose, range, setRange }: IProps) => {
     const handleSubmit = (values: IDateRange) => {
+        values.to = timestampToFormDate(Date.parse(values.to) + 86400000);
         setRange({ ...values });
         onClose();
     };

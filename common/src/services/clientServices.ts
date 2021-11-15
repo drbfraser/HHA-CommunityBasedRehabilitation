a30f417c26fc1f5b2ce868fa708f8bd22ced90df
panwangp <richard_pan@sfu.ca>
2021-11-15 15:04:50 -0800

    run prettier, and push this part One

--- common/src/services/clientServices.ts
+++ common/src/services/clientServices.ts
@@ -1,22 +1,21 @@
 import { apiFetch, APIFetchFailError, Endpoint, objectToFormData } from "../util/endpoints";
-import { RequestConfigBuilder,ConfigEntity } from  "./tools/RequestConfigBuilder";
+import { RequestConfigBuilder, ConfigEntity } from "./tools/RequestConfigBuilder";
 
 const addClient = async (clientInfo: FormData) => {
-  // const init: RequestInit = {
-  //     method: "POST",
-  //     body: clientInfo,
-  // };
+    const configEntity: ConfigEntity = RequestConfigBuilder.buildConfig(
+        "POST",
+        clientInfo,
+        Endpoint.CLIENTS,
+        ""
+    );
 
-  const configEntity : ConfigEntity = RequestConfigBuilder.buildConfig("POST", clientInfo, Endpoint.CLIENTS,"");
-
-  // return await apiFetch(Endpoint.CLIENTS, "", init)
-  return await apiFetch(configEntity.endpoint, configEntity.userId, configEntity.init)
-      .then((res) => {
-          return res.json();
-      })
-      .then((res) => {
-          return res;
-      });
+    return await apiFetch(configEntity.endpoint, configEntity.userId, configEntity.init)
+        .then((res) => {
+            return res.json();
+        })
+        .then((res) => {
+            return res;
+        });
 };
 
 export default addClient;
--- common/src/services/tools/RequestConfigBuilder.ts
+++ common/src/services/tools/RequestConfigBuilder.ts
@@ -1,35 +1,34 @@
-import { apiFetch, APIFetchFailError, Endpoint, objectToFormData } from "../../util/endpoints";
+import { Endpoint } from "../../util/endpoints";
 
 interface Config {
-  init: RequestInit,
-  userId: number,
-  endpoint: Endpoint
+    init: RequestInit;
+    userId: number;
+    endpoint: Endpoint;
 }
 
 class RequestInitEntity {
-  method:string;
-  body:any;
-  constructor(method:string, body:any) {
-    this.method = method;
-    this.body = body;
-  }
+    method: string;
+    body: any;
+    constructor(method: string, body: any) {
+        this.method = method;
+        this.body = body;
+    }
 }
 
 export class ConfigEntity {
-  userId: string;
-  endpoint: Endpoint;
-  init: RequestInit;
+    userId: string;
+    endpoint: Endpoint;
+    init: RequestInit;
 
-  constructor(userId: string, httpMethod: string, endpoint:Endpoint, body:any) {
-    this.userId = userId;
-    this.endpoint = endpoint;
-    this.init = new RequestInitEntity(httpMethod,body);
-  }
+    constructor(userId: string, httpMethod: string, endpoint: Endpoint, body: any) {
+        this.userId = userId;
+        this.endpoint = endpoint;
+        this.init = new RequestInitEntity(httpMethod, body);
+    }
 }
 
 export const RequestConfigBuilder = {
-  buildConfig: (httpMethod:string, body:any, endpoint:Endpoint ,userId:string) => {
-
-    return new ConfigEntity(userId, httpMethod, endpoint, body);
-  }
-}
+    buildConfig: (httpMethod: string, body: any, endpoint: Endpoint, userId: string) => {
+        return new ConfigEntity(userId, httpMethod, endpoint, body);
+    },
+};

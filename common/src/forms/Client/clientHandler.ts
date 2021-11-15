6cc5d75d33df30f04fcd8f80f640d10cdc2faf71
panwangp <richard_pan@sfu.ca>
2021-11-15 15:10:51 -0800

    delete unnecessary code blocks

--- common/src/forms/Client/clientHandler.ts
+++ common/src/forms/Client/clientHandler.ts
@@ -7,21 +7,6 @@ import { appendPicture, IClient } from "../../util/clients";
 import history from "../../util/history";
 import addClient from "../../services/clientServices";
 
-// const addClient = async (clientInfo: FormData) => {
-//     const init: RequestInit = {
-//         method: "POST",
-//         body: clientInfo,
-//     };
-
-//     return await apiFetch(Endpoint.CLIENTS, "", init)
-//         .then((res) => {
-//             return res.json();
-//         })
-//         .then((res) => {
-//             return res;
-//         });
-// };
-
 const updateClient = async (clientInfo: FormData, clientId: number): Promise<IClient> => {
     const init: RequestInit = {
         method: "PUT",

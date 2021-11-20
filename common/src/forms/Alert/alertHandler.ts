144bfb9341855e19d31c14db92955526d2b836d9
panwangp <richard_pan@sfu.ca>
2021-11-20 15:00:14 -0800

    I have complete ranking inbox list of alerts by creating time, next work on getUserID

--- common/src/forms/Alert/alertHandler.ts
+++ common/src/forms/Alert/alertHandler.ts
@@ -3,12 +3,14 @@ import { FormikHelpers } from "formik";
 import { apiFetch, APIFetchFailError, Endpoint, objectToFormData } from "../../util/endpoints";
 import { IAlert } from "../../util/alerts";
 import history from "../../util/history";
+import { getCurrentUser } from "../../util/hooks/currentUser";
 
 const addAlert = async (alertInfo: FormData): Promise<IAlert> => {
     const init: RequestInit = {
         method: "POST",
         body: alertInfo,
     };
+
 
     return await apiFetch(Endpoint.ALERTS, "", init)
         .then((res) => {
@@ -23,6 +25,9 @@ export const handleNewWebAlertSubmit = async (
     values: TAlertValues,
     helpers: FormikHelpers<TAlertValues>
 ) => {
+    const user = await getCurrentUser();
+    console.log("USER is : user");
+
     /*
     TODO:
     need to keep a parameter showing the userID of the user who is using the system in the top layer.
@@ -32,8 +37,9 @@ export const handleNewWebAlertSubmit = async (
         subject: values.subject,
         priority: values.priority,
         alert_message: values.alert_message,
-        created_by_user: "1",
+        created_by_user: user,
     };
+    console.log(newAlert);
     const formData = objectToFormData(newAlert);
     try {
         const alert: IAlert = await addAlert(formData);

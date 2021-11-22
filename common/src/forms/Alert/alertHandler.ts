914cac984f512f3e4be674eb768da89a187f69dd
Mike Thai <mta24@sfu.ca>
2021-11-21 18:27:49 -0800

    Add list of userIDs to track unread alerts

--- common/src/forms/Alert/alertHandler.ts
+++ common/src/forms/Alert/alertHandler.ts
@@ -4,6 +4,7 @@ import { apiFetch, APIFetchFailError, Endpoint, objectToFormData } from "../../u
 import { IAlert } from "../../util/alerts";
 import history from "../../util/history";
 import { socket } from "../../context/SocketIOContext";
+import { IUser } from "../../util/users";
 
 const addAlert = async (alertInfo: FormData): Promise<IAlert> => {
     const init: RequestInit = {
@@ -29,19 +30,38 @@ export const handleNewWebAlertSubmit = async (
     need to keep a parameter showing the userID of the user who is using the system in the top layer.
     Then this userID will be availuable for every page rendered.
   */
+    let usersList: string[];
+    try {
+        // Add all users as having unread the alert initially
+        let res = await apiFetch(Endpoint.USERS, "");
+        let users = await res.json();
+        // Reduces list of users to an array of userIDs
+        usersList = users.reduce((acc: string, value: IUser) => {
+          if (value.id && value.id != '1') { // TODO: get userID from context instead of hard coding
+            acc = acc.concat(value.id)
+          }
+          return acc
+        }, [])
+
+    } catch (e) {
+        throw new Error(`Error retreiving users. ${e}`);
+    }
     const newAlert = {
         subject: values.subject,
         priority: values.priority,
         alert_message: values.alert_message,
-        created_by_user: "1",
+        unread_by_users: usersList ? usersList : [],
+        created_by_user: "1", // TODO: get userID from context instead of hard coding
     };
+
     const formData = objectToFormData(newAlert);
     try {
         const alert: IAlert = await addAlert(formData);
         history.push("/alerts/inbox");
-        // emit socket event to the backend
-        socket.emit("newAlert", newAlert);
+        socket.emit("newAlert", newAlert); // emit socket event to the backend
+
         return alert;
+
     } catch (e) {
         const initialMessage = "Encountered an error while trying to create the alert!";
         const detailedError =

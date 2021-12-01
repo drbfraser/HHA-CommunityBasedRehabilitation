466a7d346562dd63490b976561643c6360a8019c
zjzhao <zjzhao@sfu.ca>
2021-11-30 17:23:18 -0800

    get client full name for referral conflicts

--- mobile/src/components/ConflictDialog/ConflictDialog.tsx
+++ mobile/src/components/ConflictDialog/ConflictDialog.tsx
@@ -18,29 +18,50 @@ const ConflictDialog = () => {
 
     const currState: RootState = useSelector((state: RootState) => state);
     const noConflicts: boolean = currState.conflicts.cleared;
-    const userConflicts: Object = currState.conflicts.userConflicts;
-    const clientConflicts: Object = currState.conflicts.clientConflicts;
 
-    const clients = Object.keys(clientConflicts);
-    clients.map(async (key) => {
-        if (!clientConflicts[key].name) {
-            /* Entry was added without name ie. Referral conflict */
-            const client = await database.get(modelName.clients).find(key);
-            clientConflicts[key].name = client.full_name;
-        }
-    });
+    const [clientConflicts, setClientConflicts] = useState<Object>({});
+    const [numClientConflicts, setNumClientConflicts] = useState<Number>(0);
+    const [userConflicts, setUserConflicts] = useState<Object>({});
+    const [numUserConflicts, setNumUserConflicts] = useState<Number>(0);
 
-    const [dialogVisible, setDialogVisible] = useState<boolean>(!noConflicts);
+    const [dialogVisible, setDialogVisible] = useState<boolean>(false);
+
+    const updateNames = async () => {
+        const clientIds = Object.keys(currState.conflicts.clientConflicts);
+        const clients = currState.conflicts.clientConflicts;
+        Promise.all(clientIds.map(async (key) => {
+            if (!clients[key].name) {
+                /* Entry was added without name ie. Referral conflict */
+                const client = await database.get(modelName.clients).find(key);
+                clients[key].name = client.full_name;
+            }
+        })).then(() => {
+            setClientConflicts(clients);
+            setNumClientConflicts(Object.keys(clients).length);
+            setUserConflicts(currState.conflicts.userConflicts);
+            setNumUserConflicts(Object.keys(currState.conflicts.userConflicts).length);
+        });
+    }
     
     useEffect(() => {
         if (!noConflicts) {
-            setDialogVisible(true);
+            if (numClientConflicts > 0 || numUserConflicts > 0) {
+                setDialogVisible(true);
+            } else {
+                updateNames();
+            }
         }
-    }, [currState]);
+    }, [currState, clientConflicts, userConflicts]);
 
     const onClose = () => {
         dispatch(clearSyncConflicts());
+
+        /* Reset state defaults */
         setDialogVisible(false);
+        setClientConflicts({});
+        setNumClientConflicts(0);
+        setUserConflicts({});
+        setNumUserConflicts(0);
     }
 
     return (
@@ -54,7 +75,7 @@ const ConflictDialog = () => {
                             The following changes could not be saved to the server due to change conflict.
                         </Text>
                         <ScrollView>
-                            <List.Accordion
+                            {numClientConflicts > 0 && <List.Accordion
                                 theme={{ colors: { background: themeColors.blueBgLight } }}
                                 title={"Client Conflicts"}
                             >
@@ -68,8 +89,9 @@ const ConflictDialog = () => {
                                         </View>
                                     );
                                 })}
-                            </List.Accordion>
-                            <List.Accordion
+                                <Text></Text>
+                            </List.Accordion>}
+                            {numUserConflicts > 0 && <List.Accordion
                                 theme={{ colors: { background: themeColors.blueBgLight } }}
                                 title={"User Conflicts"}
                             >
@@ -83,7 +105,8 @@ const ConflictDialog = () => {
                                         </View>
                                     );
                                 })}
-                            </List.Accordion>
+                                <Text></Text>
+                            </List.Accordion>}
                         </ScrollView>
                 </Dialog.Content>
                 <Dialog.Actions>

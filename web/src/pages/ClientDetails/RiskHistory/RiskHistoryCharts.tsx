164c8edd2cf9d0e44a74c44ef2c14769641425c4
panwangp <richard_pan@sfu.ca>
2022-03-19 17:38:11 -0700

    switch branch to do code review

--- web/src/pages/ClientDetails/RiskHistory/RiskHistoryCharts.tsx
+++ web/src/pages/ClientDetails/RiskHistory/RiskHistoryCharts.tsx
@@ -24,7 +24,6 @@ interface IChartData {
     [RiskType.HEALTH]: IDataPoint[];
     [RiskType.EDUCATION]: IDataPoint[];
     [RiskType.SOCIAL]: IDataPoint[];
-    [RiskType.NUTRITION]: IDataPoint[];
 }
 
 interface IDataPoint {
@@ -37,27 +36,32 @@ const risksToChartData = (risks: IRisk[]) => {
         [RiskType.HEALTH]: [],
         [RiskType.EDUCATION]: [],
         [RiskType.SOCIAL]: [],
-        [RiskType.NUTRITION]: [],
     };
 
     risks.sort((a, b) => a.timestamp - b.timestamp);
 
     risks.forEach((risk) => {
-        dataObj[risk.risk_type].push({
-            timestamp: risk.timestamp,
-            level: risk.risk_level,
-        });
+        // TODO: REMOVE when working on visit
+        if(risk.risk_type!==RiskType.NUTRITION) {
+          dataObj[risk.risk_type].push({
+              timestamp: risk.timestamp,
+              level: risk.risk_level,
+          });
+        }
     });
 
     // add data for the current time
-    [RiskType.HEALTH, RiskType.EDUCATION, RiskType.SOCIAL, RiskType.NUTRITION].forEach((riskType) => {
-        const riskArray = dataObj[riskType];
+    [RiskType.HEALTH, RiskType.EDUCATION, RiskType.SOCIAL].forEach((riskType) => {
+        // TODO: REMOVE when working on visit
+        if(riskType!==RiskType.NUTRITION) {
+          const riskArray = dataObj[riskType];
 
-        if (riskArray.length) {
-            riskArray.push({
-                timestamp: Date.now(),
-                level: riskArray[riskArray.length - 1].level,
-            });
+          if (riskArray.length) {
+              riskArray.push({
+                  timestamp: Date.now(),
+                  level: riskArray[riskArray.length - 1].level,
+              });
+          }
         }
     });
 
@@ -113,7 +117,10 @@ const RiskHistoryCharts = ({ client }: IProps) => {
 
     return (
         <Grid container>
-            {[RiskType.HEALTH, RiskType.EDUCATION, RiskType.SOCIAL, RiskType.NUTRITION].map((riskType) => (
+            {[RiskType.HEALTH, RiskType.EDUCATION, RiskType.SOCIAL].map((riskType) => (
+                // TODO: REMOVE when working on visit
+                (riskType!==RiskType.NUTRITION) ?
+
                 <Grid key={riskType} item md={4} xs={12}>
                     <Typography variant="h5" className={styles.textCenter}>
                         {riskTypes[riskType].name} Risk
@@ -129,6 +136,7 @@ const RiskHistoryCharts = ({ client }: IProps) => {
                         />
                     )}
                 </Grid>
+                :<></>
             ))}
         </Grid>
     );

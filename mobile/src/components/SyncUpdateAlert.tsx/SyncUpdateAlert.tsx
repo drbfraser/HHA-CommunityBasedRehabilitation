833a9be89b3eb5d978fd1f4edb3396f4e994af20
myang0 <mwyang99@gmail.com>
2022-04-07 23:52:13 -0700

    cover case edge case where both last synced version and current version do not match server version

--- mobile/src/components/SyncUpdateAlert.tsx/SyncUpdateAlert.tsx
+++ mobile/src/components/SyncUpdateAlert.tsx/SyncUpdateAlert.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { apiFetch, APIFetchFailError, Endpoint } from "@cbr/common";
+import { mobileApiVersion } from "../../util/syncHandler";
 import { Modal, Text, View, TouchableOpacity, StyleSheet, TextInput } from "react-native";
 import { themeColors } from "@cbr/common";
 
 export default function SyncUpdateAlert({ visibility, dismissAlert, onConfirm }) {
     const [syncConfirmed, setSyncConfirmed] = useState<boolean>(false);
+    const [upToDateWithServer, setUpToDateWithServer] = useState<boolean>(false);
 
     const styles = StyleSheet.create({
         disabledButton: {
@@ -16,10 +19,22 @@ export default function SyncUpdateAlert({ visibility, dismissAlert, onConfirm })
             alignItems: "center",
             backgroundColor: themeColors.blueBgDark,
         },
+        buttonText: {
+            color: "white",
+            fontSize: 16,
+        },
         modalText: {
+            fontSize: 16,
             marginTop: 10,
             paddingHorizontal: 10,
             color: "black",
+        },
+        headerText: {
+            fontSize: 28,
+            marginTop: 10,
+            fontWeight: "bold",
+            color: "black",
+            marginHorizontal: 10,
         },
     });
 
@@ -29,6 +44,33 @@ export default function SyncUpdateAlert({ visibility, dismissAlert, onConfirm })
         setSyncConfirmed(false);
         dismissAlert(false);
     };
+
+    const compareServerVersion = async () => {
+        const versionCheckBody = {
+            api_version: mobileApiVersion,
+        };
+
+        const init: RequestInit = {
+            method: "POST",
+            body: JSON.stringify(versionCheckBody),
+        };
+
+        try {
+            const response = await apiFetch(Endpoint.VERSION_CHECK, "", init);
+
+            if (response.ok) {
+                setUpToDateWithServer(true);
+            }
+        } catch (e) {
+            if (e instanceof APIFetchFailError && e.status === 403) {
+                setUpToDateWithServer(false);
+            }
+        }
+    };
+
+    useEffect(() => {
+        compareServerVersion();
+    });
 
     return (
         <View>
@@ -52,82 +94,97 @@ export default function SyncUpdateAlert({ visibility, dismissAlert, onConfirm })
                             elevation: 10,
                         }}
                     >
-                        <Text
-                            style={{
-                                fontSize: 28,
-                                marginTop: 10,
-                                fontWeight: "bold",
-                                color: "black",
-                            }}
-                        >
-                            HHA CBR is Updating!
-                        </Text>
-                        <Text style={styles.modalText}>
-                            Our servers are undergoing major changes. As a result, any old data is
-                            now obsolete and cannot be synced. If you would still like to sync, then
-                            any local changes on your device must first be destroyed.
-                        </Text>
-                        <Text style={styles.modalText}>
-                            You should use the web app to recreate your local changes, but the
-                            following will be lost:
-                        </Text>
-                        <Text style={styles.modalText}>
-                            Note that this will only affect local data, any settings you have
-                            previously will be unaffected.
-                        </Text>
-                        <Text style={styles.modalText}>
-                            This action may take awhile, and a considerable amount of cellular data
-                            may be used.
-                        </Text>
-                        <Text style={styles.modalText}>
-                            If you understand and would still like to proceed with the sync, type
-                            "clear local data" to confirm.
-                        </Text>
-                        <TextInput
-                            style={{
-                                marginTop: 10,
-                                borderWidth: 1,
-                                borderColor: themeColors.borderGray,
-                                borderRadius: 4,
-                                fontSize: 16,
-                                width: "90%",
-                                paddingVertical: 4,
-                            }}
-                            onChangeText={(text) => setSyncConfirmed(text == KEY)}
-                        />
-                        <View
-                            style={{
-                                flexDirection: "row",
-                                marginBottom: 10,
-                                marginTop: 10,
-                            }}
-                        >
-                            <View style={{ flex: 1, alignItems: "center" }}>
-                                <View style={{ width: "90%" }}>
-                                    <TouchableOpacity
-                                        style={styles.enabledButton}
-                                        onPress={() => shutdownModal()}
-                                    >
-                                        <Text style={{ color: "white" }}>CANCEL</Text>
-                                    </TouchableOpacity>
+                        {upToDateWithServer ? (
+                            <>
+                                <Text style={styles.headerText}>HHA CBR is Updating!</Text>
+                                <Text style={styles.modalText}>
+                                    Our servers are undergoing major changes. As a result, any old
+                                    data is now obsolete and cannot be synced. If you would still
+                                    like to sync, then any local changes on your device must first
+                                    be destroyed.
+                                </Text>
+                                <Text style={styles.modalText}>
+                                    You should use the web app to recreate any local changes.
+                                </Text>
+                                <Text style={styles.modalText}>
+                                    Note that this will only affect local data, any settings that
+                                    have previously been set will be unaffected.
+                                </Text>
+                                <Text style={styles.modalText}>
+                                    This action may take awhile, and a considerable amount of
+                                    cellular data may be used.
+                                </Text>
+                                <Text style={styles.modalText}>
+                                    If you understand and would still like to proceed with the sync,
+                                    type "clear local data" to confirm.
+                                </Text>
+                                <TextInput
+                                    style={{
+                                        marginTop: 20,
+                                        marginBottom: 10,
+                                        borderWidth: 1,
+                                        borderColor: themeColors.borderGray,
+                                        borderRadius: 4,
+                                        fontSize: 16,
+                                        width: "90%",
+                                        paddingVertical: 4,
+                                    }}
+                                    onChangeText={(text) => setSyncConfirmed(text == KEY)}
+                                />
+                                <View
+                                    style={{
+                                        flexDirection: "row",
+                                        marginBottom: 10,
+                                        marginTop: 10,
+                                    }}
+                                >
+                                    <View style={{ flex: 1, alignItems: "center" }}>
+                                        <View style={{ width: "90%" }}>
+                                            <TouchableOpacity
+                                                style={styles.enabledButton}
+                                                onPress={() => shutdownModal()}
+                                            >
+                                                <Text style={styles.buttonText}>CANCEL</Text>
+                                            </TouchableOpacity>
+                                        </View>
+                                    </View>
+                                    <View style={{ flex: 1, alignItems: "center" }}>
+                                        <View style={{ width: "90%" }}>
+                                            <TouchableOpacity
+                                                style={
+                                                    syncConfirmed
+                                                        ? styles.enabledButton
+                                                        : styles.disabledButton
+                                                }
+                                                disabled={!syncConfirmed}
+                                                onPress={() => onConfirm()}
+                                            >
+                                                <Text style={styles.buttonText}>CONFIRM</Text>
+                                            </TouchableOpacity>
+                                        </View>
+                                    </View>
                                 </View>
-                            </View>
-                            <View style={{ flex: 1, alignItems: "center" }}>
-                                <View style={{ width: "90%" }}>
-                                    <TouchableOpacity
-                                        style={
-                                            syncConfirmed
-                                                ? styles.enabledButton
-                                                : styles.disabledButton
-                                        }
-                                        disabled={!syncConfirmed}
-                                        onPress={() => onConfirm()}
-                                    >
-                                        <Text style={{ color: "white" }}>CONFIRM</Text>
-                                    </TouchableOpacity>
-                                </View>
-                            </View>
-                        </View>
+                            </>
+                        ) : (
+                            <>
+                                <Text style={styles.headerText}>
+                                    Your Version of HHA CBR is Out of Date
+                                </Text>
+                                <Text style={styles.modalText}>
+                                    A new version of HHA CBR is available on the Google Play Store.
+                                    Please update your app to be able to use this feature.
+                                </Text>
+                                <TouchableOpacity
+                                    style={[
+                                        styles.enabledButton,
+                                        { width: 100, marginVertical: 10 },
+                                    ]}
+                                    onPress={() => shutdownModal()}
+                                >
+                                    <Text style={styles.buttonText}>OK</Text>
+                                </TouchableOpacity>
+                            </>
+                        )}
                     </View>
                 </View>
             </Modal>

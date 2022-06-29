2443b624bba994da13cc0e93a9373ede61b827d5
Lukas Kuppers <lkuppers@sfu.ca>
2022-06-29 11:23:35 -0700

    Create layout for switch server screen

--- mobile/src/screens/SwitchServer/SwitchServer.styles.ts
+++ mobile/src/screens/SwitchServer/SwitchServer.styles.ts
@@ -1,13 +1,66 @@
-import { StyleSheet, useWindowDimensions } from "react-native";
-import { useTheme } from "react-native-paper";
+import { StyleSheet } from "react-native";
+import { themeColors } from "@cbr/common";
+import theme from "../../util/theme.styles";
 
 const useStyles = () => {
-    const theme = useTheme();
 
     return StyleSheet.create({
         container: {
             flex: 1, 
-            marginHorizontal: 5
+        }, 
+        groupContainer: {
+            padding: 10
+        }, 
+        cardSectionTitle: {
+            textAlign: "center",
+            marginTop: 14,
+            marginBottom: 14,
+            fontSize: 28,
+            color: themeColors.blueBgDark,
+        }, 
+        CardStyle: {
+            display: "flex",
+            padding: 20,
+            borderRadius: 20,
+            marginBottom: 10,
+        }, 
+        row: {
+            flexDirection: "row",
+            paddingTop: 5,
+            paddingBottom: 5,
+            justifyContent: "space-between",
+        }, 
+        chipText: {
+            color: themeColors.white
+        },
+        chipLive: {
+            backgroundColor: themeColors.hhaGreen, 
+            height: 35
+        }, 
+        chipTest: {
+            backgroundColor: themeColors.riskYellow, 
+            height: 35
+        }, 
+        radioButtonPassive: {
+            marginBottom: 10, 
+            height: 60, 
+            justifyContent: "center", 
+            borderRadius: 10,
+            borderWidth: 2, 
+            backgroundColor: themeColors.white,
+            borderColor: theme.colors.backdrop
+        }, 
+        radioButtonSelected: {
+            marginBottom: 10, 
+            height: 60, 
+            justifyContent: "center", 
+            borderRadius: 10, 
+            borderWidth: 2, 
+            backgroundColor: themeColors.white, 
+            borderColor: themeColors.hhaGreen
+        }, 
+        switchServerButton: {
+            marginTop: 50
         }
     });
 }
--- mobile/src/screens/SwitchServer/SwitchServer.tsx
+++ mobile/src/screens/SwitchServer/SwitchServer.tsx
@@ -1,15 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import useStyles from "./SwitchServer.styles";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { Text } from "react-native-paper";
+import { Text, Card, Chip, Button, TextInput } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from "react-native-gesture-handler";
+import { View } from "react-native";
 
 const SwitchServer = () => {
+    enum ServerOption {
+        LIVE = "Live", 
+        TEST = "Test", 
+        NONE = "None"
+    }
+
     const styles = useStyles();
+    const [selectedServer, setSelectedServer] = useState(ServerOption.NONE);
+    const [testServerURL, setTestServerURL] = useState("");
+
+    const switchServer = (server: ServerOption) => {
+        alert("Switching to server: " + server);
+    }
+
+    const radioButton = (value: ServerOption) => {
+        const style = value === selectedServer ? styles.radioButtonSelected : styles.radioButtonPassive;
+        return (
+            <Button
+                style={style}
+                mode="outlined"
+                icon={ value === selectedServer ? "check" : "" }
+                onPress={ () => setSelectedServer(value) }
+            >
+                {value} Server
+            </Button>
+        )
+    }
 
     return (
-        <KeyboardAwareScrollView style={styles.container}>
-            <Text>This is the switch server screen</Text>
-        </KeyboardAwareScrollView>
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.groupContainer}>
+                <Text style={styles.cardSectionTitle}>Current Status</Text>
+                <Card style={styles.CardStyle}>
+                    <View style={styles.row}>
+                        <Text>Connected to server: </Text>
+                        <Chip 
+                            textStyle={styles.chipText}
+                            style={styles.chipLive}>Live Server</Chip>
+                    </View>
+                    <View style={styles.row}>
+                        <Text>Server URL: </Text>
+                        <Text>https://testurl.com</Text>
+                    </View>
+                </Card>
+
+                <Text style={styles.cardSectionTitle}>Select Server</Text>
+                <View>
+                    {radioButton(ServerOption.LIVE)}
+                    {radioButton(ServerOption.TEST)}
+                </View>
+                { selectedServer === ServerOption.TEST ? (
+                    <View>
+                        <TextInput 
+                            label="Test Server URL"
+                            error={false}
+                            value={testServerURL}
+                            onChangeText={(newURL) => setTestServerURL(newURL)}
+                            mode="outlined"
+                            blurOnSubmit={false}
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            autoCompleteType="off"
+                            textContentType="URL"
+                            returnKeyType="next"
+                            onSubmitEditing={ () => switchServer(selectedServer) }
+                        />
+                    </View>
+                ) : (<View></View>)}
+
+                <Button 
+                    style={styles.switchServerButton}
+                    mode="contained"
+                    onPress={ () => switchServer(selectedServer) }
+                >
+                    Switch Servers
+                </Button>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 

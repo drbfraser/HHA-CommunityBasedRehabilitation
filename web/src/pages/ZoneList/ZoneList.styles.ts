9c3ab601614bc41dc42ca454e2948866acfa30a1
mpa72 <mpa72@sfu.ca>
2022-07-06 00:45:23 +0000

    Resolve "Add ability to create Zones,"

---
+++ web/src/pages/ZoneList/ZoneList.styles.ts
@@ -0,0 +1,45 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { mediaCompressedDataGrid } from "styles/DataGrid.styles";
+import { mediaMobile } from "theme.styles";
+
+export const useStyles = makeStyles(
+    {
+        container: {
+            height: "calc(100vh - 175px)",
+            minHeight: 400,
+            padding: "5px 0px 25px 0px",
+        },
+        topContainer: {
+            justifyContent: "flex-end",
+            display: "flex",
+        },
+        dataGridWrapper: {
+            height: "100%",
+            width: "100%",
+        },
+        icon: {
+            padding: "0px 15px 0px 0px",
+        },
+        [mediaMobile]: {
+            container: {
+                height: "calc(100vh - 150px)",
+                paddingBottom: "50px",
+            },
+        },
+        [mediaCompressedDataGrid]: {
+            container: {
+                paddingBottom: "71px",
+            },
+            topContainer: {
+                flexGrow: 1,
+                justifyContent: "center",
+            },
+            dataGridWrapper: {
+                height: "100%",
+                width: "100%",
+                marginTop: 0,
+            },
+        },
+    },
+    { index: 1 }
+);

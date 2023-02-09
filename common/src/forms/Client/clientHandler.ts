f3d395ad80fa78ac06c361335bce0cad6c20db15
Liam Thorne <lnt6@sfu.ca>
2023-02-09 14:15:38 -0800

    add message for archived clients

--- common/src/forms/Client/clientHandler.ts
+++ common/src/forms/Client/clientHandler.ts
@@ -164,7 +164,7 @@ export const handleArchiveConfirmation = (
         window.confirm(
             `Are you sure you want to ${values.is_active ? "archive" : "dearchive"} ${
                 values.first_name
-            } ${values.last_name}?\n`
+            } ${values.last_name}?\nYou cannot edit clients while they are archived`
         )
     ) {
         // set is_active

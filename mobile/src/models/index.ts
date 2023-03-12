fea4cb7afc83a0f5c2d51bbd02534844e55b7e70
Sam Kim <jhk29@sfu.ca>
2023-03-12 01:58:42 -0800

    109: Temporary save for frontend mobile changes. Currently running into a bug where pullChanges doesn't pull to alert table

--- mobile/src/models/index.ts
+++ mobile/src/models/index.ts
@@ -6,5 +6,6 @@ import User from "./UserCBR";
 import Referral from "./Referral";
 import BaselineSurvey from "./BaselineSurvey";
 import Visit from "./Visit";
+import Alert from "./Alert";
 
-export const dbModels = [User, Client, Risk, Referral, BaselineSurvey, Visit, Outcome, Improvement];
+export const dbModels = [User, Client, Risk, Referral, BaselineSurvey, Visit, Outcome, Improvement, Alert];

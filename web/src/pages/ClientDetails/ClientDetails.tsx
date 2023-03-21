0924f202aa023573404b6137be3c2252412ce6a1
Liam Thorne <lnt6@sfu.ca>
2023-03-21 15:22:53 -0700

    Fix ESLint warning by making function useCallback

--- web/src/pages/ClientDetails/ClientDetails.tsx
+++ web/src/pages/ClientDetails/ClientDetails.tsx
@@ -25,7 +25,7 @@ const ClientDetails = () => {
 
     const history = useHistory();
 
-    const getClient = () => {
+    const getClient = useCallback(() => {
         apiFetch(Endpoint.CLIENT, `${clientId}`)
             .then((resp) => resp.json())
             .then((client: IClient) => {
@@ -34,11 +34,11 @@ const ClientDetails = () => {
                 setClientInfo(client);
             })
             .catch(() => setLoadingError(true));
-    };
+    }, [clientId]);
 
     useEffect(() => {
         getClient();
-    }, [clientId]);
+    }, [getClient]);
     return loadingError ? (
         <Alert severity="error">Something went wrong loading that client. Please try again.</Alert>
     ) : (

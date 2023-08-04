23df46ccf4240ef69b0db89bfbb6b464c6316c34
Brian Fraser <bfraser@sfu.ca>
2023-08-04 13:44:29 -0700

    Update monthly_backup_script.sh
    Add backup of .env

--- scripts/monthly_backup_script.sh
+++ scripts/monthly_backup_script.sh
@@ -4,6 +4,8 @@
 source "$(dirname "$(realpath "$0")")/../.env"
 BACKUP_FILENAME="cbr_backup_zip_$(date +%Y%m%d_%H%M%S).tar.gz"
 
+cp "$(dirname "$(realpath "$0")")/../.env" "$S3_BACKUP_SOURCE_DIR"
+
 tar -czvf "$BACKUP_FILENAME" -C "$S3_BACKUP_SOURCE_DIR" .
 
 aws s3 cp "$BACKUP_FILENAME" "s3://$S3_BUCKET_NAME/monthly/"

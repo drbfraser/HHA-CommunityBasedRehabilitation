866c54f1bf4bf5e879c60530702fbf4f596936e8
Harry Nguyen <hln5@sfu.ca>
2024-09-19 18:58:35 -0700

    cleanup imports in App.tsx

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,12 +1,15 @@
-import React, { createContext, useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Provider as PaperProvider } from "react-native-paper";
+import { enableScreens } from "react-native-screens";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavigationContainer } from "@react-navigation/native";
-import theme from "./util/theme.styles";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaView } from "react-native-safe-area-context";
-import globalStyle from "./app.styles";
-import { stackScreenOptions, stackScreenProps } from "./util/stackScreens";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { Provider as StoreProvider } from "react-redux";
+import { io } from "socket.io-client/dist/socket.io";
+import DatabaseProvider from "@nozbe/watermelondb/DatabaseProvider";
+import i18next from "i18next";
+
 import {
     APILoadError,
     commonConfiguration,
@@ -16,31 +19,24 @@ import {
     getCurrentUser,
     invalidateAllCachedAPI,
     isLoggedIn,
-    testFn,
     initI18n,
 } from "@cbr/common";
+import theme from "./util/theme.styles";
+import globalStyle from "./app.styles";
+import { stackScreenOptions, stackScreenProps } from "./util/stackScreens";
 import { AuthContext as AuthContext, IAuthContext } from "./context/AuthContext/AuthContext";
-import { enableScreens } from "react-native-screens";
 import Loading from "./screens/Loading/Loading";
 import { AuthState } from "./context/AuthContext/AuthState";
 import { CacheRefreshTask } from "./tasks/CacheRefreshTask";
 import { StackScreenName, NoAuthScreenName } from "./util/StackScreenName";
-import DatabaseProvider from "@nozbe/watermelondb/DatabaseProvider";
 import { database } from "./util/watermelonDatabase";
-import { io } from "socket.io-client/dist/socket.io";
 import { SyncDatabaseTask } from "./tasks/SyncDatabaseTask";
 import { SyncContext } from "./context/SyncContext/SyncContext";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { SyncSettings } from "./screens/Sync/PrefConstants";
 import { AutoSyncDB } from "./util/syncHandler";
-import { Provider as StoreProvider } from "react-redux";
 import { store } from "./redux/store";
-import i18next from "i18next";
-// import "./i18n.config";
 
-// initI18n(i18next);
-testFn();
-console.log("new code!");
+initI18n(i18next);
 
 // Ensure we use FragmentActivity on Android
 // https://reactnavigation.org/docs/react-native-screens
@@ -48,7 +44,6 @@ enableScreens();
 
 const Stack = createStackNavigator();
 const styles = globalStyle();
-const Tab = createMaterialBottomTabNavigator();
 
 /**
  * @see IAuthContext#requireLoggedIn
@@ -120,10 +115,6 @@ export default function App() {
     const [autoSync, setAutoSync] = useState<boolean>(true);
     const [cellularSync, setCellularSync] = useState<boolean>(false);
     const [screenRefresh, setScreenRefresh] = useState<boolean>(false);
-
-    useEffect(() => {
-        initI18n(i18next);
-    }, []);
 
     useEffect(() => {
         // Refresh disabilities, zones, current user information

5dec15554eee3f4780924e4074b66e35217b63e9
Stokely Diamantis <sda168@sfu.ca>
2024-09-23 22:36:25 -0700

    add i18n declaration file to mobile project

---
+++ mobile/src/@types/react-i18next.d.ts
@@ -0,0 +1,16 @@
+// todo: is it possible to just use the typing in common?
+
+import "i18next";
+
+import en from '../../../locales/en.json';
+import bari from '../../../locales/bari.json';
+
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    defaultNS: 'en';
+    resources: {
+      en: typeof en;
+      bari: typeof bari;
+    };
+  }
+}

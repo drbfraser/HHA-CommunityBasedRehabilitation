ee23e7971c2a8655d6c394471d26459a8e26f3f6
Stokely Diamantis <sda168@sfu.ca>
2024-09-25 23:34:59 -0700

    prettier format

--- common/src/@types/i18next.d.ts
+++ common/src/@types/i18next.d.ts
@@ -1,10 +1,10 @@
 import "i18next";
 
-import Resources from './resources.ts';
+import Resources from "./resources.ts";
 
-declare module 'i18next' {
-  interface CustomTypeOptions {
-    defaultNS: 'en';
-    resources: Resources;
-  }
+declare module "i18next" {
+    interface CustomTypeOptions {
+        defaultNS: "en";
+        resources: Resources;
+    }
 }

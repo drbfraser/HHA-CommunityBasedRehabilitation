9f7bcff68ee487838cdddd541c2447f947f991da
Harry Nguyen <hln5@sfu.ca>
2024-09-26 20:07:24 -0700

    refactor(side nav): cleanup code

--- web/src/components/SideNav/SideNavIcon.tsx
+++ web/src/components/SideNav/SideNavIcon.tsx
@@ -1,32 +1,33 @@
-import { Badge } from "@material-ui/core";
-import Tooltip from "@material-ui/core/Tooltip";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { IPage } from "util/pages";
-import { useStyles } from "./SideNav.styles";
+import { Badge } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+import Tooltip from "@material-ui/core/Tooltip";
+
 import { IAlert } from "@cbr/common/util/alerts";
 import { apiFetch, APILoadError, Endpoint } from "@cbr/common/util/endpoints";
-import { Alert } from "@material-ui/lab";
 import { socket } from "@cbr/common/context/SocketIOContext";
 import { IUser } from "@cbr/common/util/users";
 import { getCurrentUser } from "@cbr/common/util/hooks/currentUser";
+import { IPage } from "util/pages";
+import { useStyles } from "./SideNav.styles";
 
 interface IProps {
     page: IPage;
     active: boolean;
 }
 
-const SideNavIcon = ({ page, active }: IProps) => {
+const SideNavIcon = ({ page, active: iconIsActive }: IProps) => {
     const styles = useStyles();
-    // fix issue with findDOMNode in strict mode
-    const NoTransition = ({ children }: any) => children;
     const [unreadAlertsCount, setUnreadAlertsCount] = useState<number>(0);
     const [isUnreadAlertCountSet, setIsUnreadAlertCountSet] = useState<boolean>(false);
+
+    // fix issue with findDOMNode in strict mode
+    const NoTransition = ({ children }: any) => children;
 
     socket.on("broadcastAlert", () => {
         fetchAlerts();
     });
-
     socket.on("updateUnreadList", (unreadAlertsCount) => {
         setUnreadAlertsCount(unreadAlertsCount);
     });
@@ -38,15 +39,17 @@ const SideNavIcon = ({ page, active }: IProps) => {
             const user: IUser | typeof APILoadError = await getCurrentUser();
 
             if (user === APILoadError || user === undefined) {
+                // TODO: translate
+                // also not sure if this even does anything (its not being returned)
                 <Alert severity="error">Something went wrong. Please go back and try again.</Alert>;
             } else {
-                let unreadAlerts: IAlert[] = alertsList.filter((alert: IAlert) =>
+                const unreadAlerts: IAlert[] = alertsList.filter((alert: IAlert) =>
                     alert.unread_by_users.includes(user.id)
                 );
                 setUnreadAlertsCount(unreadAlerts.length);
             }
         } catch (e) {
-            console.log(`Error fetching Alerts: ${e}`);
+            console.error(`Error fetching Alerts: ${e}`);
         }
     };
 
@@ -58,24 +61,26 @@ const SideNavIcon = ({ page, active }: IProps) => {
     }, [isUnreadAlertCountSet]);
 
     function IconInfo(props: any) {
+        const { path, name, Icon } = props.page;
+
         return (
-            <Link to={props.page.path}>
+            <Link to={path}>
                 <Tooltip
-                    title={props.page.name}
+                    title={name}
                     placement="top"
                     arrow
                     classes={{ tooltip: styles.tooltip }}
                     TransitionComponent={NoTransition}
                 >
-                    <div className={styles.icon + (active ? ` ${styles.active}` : "")}>
-                        {props.page.Icon && <props.page.Icon fontSize="large" />}
+                    <div className={`${styles.icon} ${iconIsActive ? styles.active : ""}`}>
+                        {Icon && <Icon fontSize="large" />}
                     </div>
                 </Tooltip>
             </Link>
         );
     }
 
-    return page.name === "Inbox" ? (
+    return page.name === "screenNames.inbox" ? (
         <Badge
             badgeContent={unreadAlertsCount}
             max={9}

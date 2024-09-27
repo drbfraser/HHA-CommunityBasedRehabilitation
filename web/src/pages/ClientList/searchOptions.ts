af118f94db643e6359a1013b511ee5edda4bdfcf
Harry Nguyen <hln5@sfu.ca>
2024-09-26 18:03:03 -0700

    feat(admin): translate dropdown select in users list

--- web/src/pages/ClientList/searchOptions.ts
+++ web/src/pages/ClientList/searchOptions.ts
@@ -1,4 +1,11 @@
+import { TFunction } from "i18next";
+
 export enum SearchOption {
     NAME = "Name",
     ZONE = "Zone",
 }
+
+export const getClientListSearchOptions = (t: TFunction) => ({
+    NAME: { value: "NAME", display: t("general.name") },
+    ZONE: { value: "ZONE", display: t("general.zone") },
+});

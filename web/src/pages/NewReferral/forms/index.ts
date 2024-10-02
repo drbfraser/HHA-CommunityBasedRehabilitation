909372f93dcab9136e4f49868f01baaf51bbebfa
Harry Nguyen <hln5@sfu.ca>
2024-10-01 18:30:25 -0700

    feat(newReferralForm): translate and extract referral service sub form

--- web/src/pages/NewReferral/forms/index.ts
+++ web/src/pages/NewReferral/forms/index.ts
@@ -3,4 +3,5 @@ export { default as NutritionForm } from "./NutritionForm";
 export { default as OtherServicesForm } from "./OtherServicesForm";
 export { default as PhysiotherapyForm } from "./PhysiotherapyForm";
 export { default as ProstheticOrthoticForm } from "./ProstheticOrthoticForm";
+export { default as ReferralServiceForm } from "./ReferralServiceForm";
 export { default as WheelchairForm } from "./WheelchairForm";

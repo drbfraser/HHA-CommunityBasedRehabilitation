50835d1b014d5edcc62941483dec0a42cc91bfb7
Harry Nguyen <hln5@sfu.ca>
2024-10-01 17:49:12 -0700

    feat(clientTimeline): translate

--- web/src/pages/ClientDetails/ClientTimeline/ReferralEntry.tsx
+++ web/src/pages/ClientDetails/ClientTimeline/ReferralEntry.tsx
@@ -1,3 +1,8 @@
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Field, Form, Formik } from "formik";
+import { TextField } from "formik-material-ui";
+import * as Yup from "yup";
 import {
     Accordion,
     AccordionDetails,
@@ -10,24 +15,21 @@ import {
     DialogTitle,
     Typography,
 } from "@mui/material";
-import React, { useState } from "react";
+import ScheduleIcon from "@material-ui/icons/Schedule";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 import {
     IReferral,
     orthoticInjuryLocations,
     prostheticInjuryLocations,
     wheelchairExperiences,
 } from "@cbr/common/util/referrals";
-import TimelineEntry from "../Timeline/TimelineEntry";
-import CheckCircleIcon from "@material-ui/icons/CheckCircle";
-import ScheduleIcon from "@material-ui/icons/Schedule";
-import { useStyles } from "./Entry.styles";
-import { timestampToDateTime } from "@cbr/common/util/dates";
-import NearMeIcon from "@material-ui/icons/NearMe";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { Field, Form, Formik } from "formik";
-import { TextField } from "formik-material-ui";
-import * as Yup from "yup";
 import { apiFetch, Endpoint } from "@cbr/common/util/endpoints";
+import { timestampToDateTime } from "@cbr/common/util/dates";
+import TimelineEntry from "../Timeline/TimelineEntry";
+import { useStyles } from "./Entry.styles";
 import { Thumb } from "components/ReferralPhotoView/Thumb";
 
 interface IEntryProps {
@@ -37,8 +39,10 @@ interface IEntryProps {
 }
 
 const ReferralEntry = ({ referral, refreshClient, dateFormatter }: IEntryProps) => {
+    const { t } = useTranslation();
     const [open, setOpen] = useState(false);
     const styles = useStyles();
+
     const Summary = ({ clickable }: { clickable: boolean }) => {
         const ReasonChip = ({ label }: { label: string }) => (
             <Chip label={label} clickable={clickable} color="primary" variant="outlined" />
@@ -51,14 +55,17 @@ const ReferralEntry = ({ referral, refreshClient, dateFormatter }: IEntryProps) 
                 ) : (
                     <ScheduleIcon fontSize="small" className={styles.pendingIcon} />
                 )}{" "}
-                <b>Referral</b> {referral.resolved ? "Resolved" : "Pending"}{" "}
-                {referral.wheelchair && <ReasonChip label="Wheelchair" />}{" "}
-                {referral.physiotherapy && <ReasonChip label="Physiotherapy" />}{" "}
-                {referral.prosthetic && <ReasonChip label="Prosthetic" />}{" "}
-                {referral.orthotic && <ReasonChip label="Orthotic" />}{" "}
-                {referral.hha_nutrition_and_agriculture_project && <ReasonChip label="Nutrition" />}{" "}
-                {referral.mental_health && <ReasonChip label="Mental" />}{" "}
-                {referral.services_other && <ReasonChip label="Other" />}{" "}
+                {/* TODO: translate "Refferal" and "pending" */}
+                <b>Referral</b> {referral.resolved ? t("general.resolved") : "Pending"}{" "}
+                {referral.wheelchair && <ReasonChip label={t("referral.wheelchair")} />}{" "}
+                {referral.physiotherapy && <ReasonChip label={t("referral.physiotherapy")} />}{" "}
+                {referral.prosthetic && <ReasonChip label={t("referral.prosthetic")} />}{" "}
+                {referral.orthotic && <ReasonChip label={t("referral.orthotic")} />}{" "}
+                {referral.hha_nutrition_and_agriculture_project && (
+                    <ReasonChip label={t("general.nutrition")} />
+                )}{" "}
+                {referral.mental_health && <ReasonChip label={t("referral.mental")} />}{" "}
+                {referral.services_other && <ReasonChip label={t("referral.other")} />}{" "}
             </>
         );
     };
@@ -93,7 +100,7 @@ const ReferralEntry = ({ referral, refreshClient, dateFormatter }: IEntryProps) 
             <Accordion variant="outlined">
                 <AccordionSummary expandIcon={<ExpandMoreIcon />}>
                     <Typography>
-                        <b>Resolve</b>
+                        <b>{t("general.resolve")}</b>
                     </Typography>
                 </AccordionSummary>
                 <AccordionDetails className={styles.resolveAccordion}>
@@ -114,7 +121,7 @@ const ReferralEntry = ({ referral, refreshClient, dateFormatter }: IEntryProps) 
                             <br />
                             <br />
                             <Button type="submit" variant="outlined" className={styles.resolveBtn}>
-                                Mark Resolved
+                                {t("referralAttr.markResolved")}
                             </Button>
                         </Form>
                     </Formik>
@@ -123,98 +130,114 @@ const ReferralEntry = ({ referral, refreshClient, dateFormatter }: IEntryProps) 
         );
     };
 
-    const ReferralDialog = () => (
-        <Dialog fullWidth maxWidth="sm" open={open} onClose={() => setOpen(false)}>
-            <DialogTitle>
-                <Summary clickable={false} />
-            </DialogTitle>
-            <DialogContent>
-                <b>Referral Date:</b> {timestampToDateTime(referral.date_referred)}
-                <br />
-                <br />
-                {referral.resolved && (
-                    <>
-                        <b>Resolution Date:</b> {timestampToDateTime(referral.date_resolved)}
-                        <br />
-                        <b>Outcome:</b> {referral.outcome}
-                        <br />
-                        <br />
-                    </>
-                )}
-                {referral.wheelchair && (
-                    <>
-                        <b>Wheelchair Experience: </b>
-                        {wheelchairExperiences[referral.wheelchair_experience]}
-                        <br />
-                        <b>Hip Width:</b> {referral.hip_width} inches
-                        <br />
-                        <b>Wheelchair Owned?</b> {referral.wheelchair_owned ? "Yes" : "No"}
-                        <br />
-                        <b>Wheelchair Repairable?</b>{" "}
-                        {referral.wheelchair_repairable ? "Yes" : "No"}
-                        <br />
-                        <Thumb Id={referral.id} Url={referral.picture}></Thumb>
-                        <br />
-                    </>
-                )}
-                {referral.physiotherapy && (
-                    <>
-                        <b>Physiotherapy Condition:</b> {referral.condition}
-                        <br />
-                        <br />
-                    </>
-                )}
-                {referral.prosthetic && (
-                    <>
-                        <b>Prosthetic Injury Location: </b>
-                        {prostheticInjuryLocations[referral.prosthetic_injury_location]}
-                        <br />
-                        <br />
-                    </>
-                )}
-                {referral.orthotic && (
-                    <>
-                        <b>Orthotic Injury Location: </b>
-                        {orthoticInjuryLocations[referral.orthotic_injury_location]}
-                        <br />
-                        <br />
-                    </>
-                )}
-                {referral.hha_nutrition_and_agriculture_project && (
-                    <>
-                        <b>Emergency Food Aid Required: </b>
-                        {referral.emergency_food_aid ? "Yes" : "No"}
-                        <br />
-                        <b>Agriculture Livelihood Program Enrollment Required: </b>
-                        {referral.agriculture_livelihood_program_enrollment ? "Yes" : "No"}
-                        <br />
-                        <br />
-                    </>
-                )}
-                {referral.mental_health && (
-                    <>
-                        <b>Mental Health Condition: </b>
-                        {referral.mental_health_condition}
-                        <br />
-                        <br />
-                    </>
-                )}
-                {Boolean(referral.services_other.trim().length) && (
-                    <>
-                        <b>Other Services Required:</b> {referral.services_other}
-                        <br />
-                        <br />
-                    </>
-                )}
-                {!referral.resolved && <ResolveForm />}
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={() => setOpen(false)} color="primary">
-                    Close
-                </Button>
-            </DialogActions>
-        </Dialog>
-    );
+    const ReferralDialog = () => {
+        const translatedYes = t("general.yes");
+        const translatedNo = t("general.no");
+
+        return (
+            <Dialog fullWidth maxWidth="sm" open={open} onClose={() => setOpen(false)}>
+                <DialogTitle>
+                    <Summary clickable={false} />
+                </DialogTitle>
+                <DialogContent>
+                    <b>{t("referralAttr.referralDate")}:</b>{" "}
+                    {timestampToDateTime(referral.date_referred)}
+                    <br />
+                    <br />
+                    {referral.resolved && (
+                        <>
+                            <b>{t("referralAttr.resolutionDate")}:</b>{" "}
+                            {timestampToDateTime(referral.date_resolved)}
+                            <br />
+                            <b>{t("general.outcome")}:</b> {referral.outcome}
+                            <br />
+                            <br />
+                        </>
+                    )}
+                    {referral.wheelchair && (
+                        <>
+                            <b>{t("referral.wheelchairExperience")}: </b>
+                            {wheelchairExperiences[referral.wheelchair_experience]}
+                            <br />
+                            <b>{t("referral.hipWidth")}:</b> {referral.hip_width}{" "}
+                            {t("referral.inches")}
+                            <br />
+                            <b>{t("referral.ownership")}?</b>{" "}
+                            {referral.wheelchair_owned ? translatedYes : translatedNo}
+                            <br />
+                            <b>{t("referral.repairability")}?</b>{" "}
+                            {referral.wheelchair_repairable ? translatedYes : translatedNo}
+                            <br />
+                            <Thumb Id={referral.id} Url={referral.picture}></Thumb>
+                            <br />
+                        </>
+                    )}
+                    {referral.physiotherapy && (
+                        <>
+                            {/* TODO: translate referral.condition */}
+                            <b>{t("referralAttr.condition_physiotherapy")}:</b> {referral.condition}
+                            <br />
+                            <br />
+                        </>
+                    )}
+                    {referral.prosthetic && (
+                        <>
+                            <b>{t("referralAttr.injuryLocation_prosthetic")}: </b>
+                            {prostheticInjuryLocations[referral.prosthetic_injury_location]}
+                            <br />
+                            <br />
+                        </>
+                    )}
+                    {referral.orthotic && (
+                        <>
+                            <b>{t("referralAttr.injuryLocation_orthotic")}: </b>
+                            {orthoticInjuryLocations[referral.orthotic_injury_location]}
+                            <br />
+                            <br />
+                        </>
+                    )}
+                    {referral.hha_nutrition_and_agriculture_project && (
+                        <>
+                            {/* TODO: translate */}
+                            <b>Emergency Food Aid Required: </b>
+                            {referral.emergency_food_aid ? translatedYes : translatedNo}
+                            <br />
+                            {/* TODO: translate */}
+                            <b>Agriculture Livelihood Program Enrollment Required: </b>
+                            {referral.agriculture_livelihood_program_enrollment
+                                ? translatedYes
+                                : translatedNo}
+                            <br />
+                            <br />
+                        </>
+                    )}
+                    {referral.mental_health && (
+                        <>
+                            <b>{t("referralAttr.condition_mental")}: </b>
+                            {/* TODO: translate */}
+                            {referral.mental_health_condition}
+                            <br />
+                            <br />
+                        </>
+                    )}
+                    {Boolean(referral.services_other.trim().length) && (
+                        <>
+                            <b>{t("referralAttr.otherServiceRequired")}:</b> {/* TODO: translate */}
+                            {referral.services_other}
+                            <br />
+                            <br />
+                        </>
+                    )}
+                    {!referral.resolved && <ResolveForm />}
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)} color="primary">
+                        {t("general.close")}
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        );
+    };
 
     return (
         <>

82a363a66e86bcee619e7c9ed364a24ad09c0384
Harry Nguyen <hln5@sfu.ca>
2024-10-01 18:24:49 -0700

    refactor(newReferralForm): add index.ts file for easier imports

---
+++ web/src/pages/NewReferral/forms/index.ts
@@ -0,0 +1,6 @@
+export { default as MentalHealthForm } from "./MentalHealthForm";
+export { default as NutritionForm } from "./NutritionForm";
+export { default as OtherServicesForm } from "./OtherServicesForm";
+export { default as PhysiotherapyForm } from "./PhysiotherapyForm";
+export { default as ProstheticOrthoticForm } from "./ProstheticOrthoticForm";
+export { default as WheelchairForm } from "./WheelchairForm";

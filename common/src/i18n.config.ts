0839ae43c31be13beb6eb057abd0881a00a56189
Harry Nguyen <hln5@sfu.ca>
2024-10-03 15:07:16 -0700

    chore(translations): remove unused import

--- common/src/i18n.config.ts
+++ common/src/i18n.config.ts
@@ -1,4 +1,4 @@
-import i18n, { ThirdPartyModule } from "i18next";
+import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./locales/en.json";
 import bari from "./locales/bari.json";

e5c13fa761dbcb8f37a1d9479a4b69f9774e72ba
Harry Nguyen <hln5@sfu.ca>
2024-10-11 11:36:35 -0700

    feat(language picker): able to change i18n language upon select

--- web/src/components/LanguagePicker/LanguagePicker.tsx
+++ web/src/components/LanguagePicker/LanguagePicker.tsx
@@ -1,26 +1,31 @@
 import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
 
 const LanguagePicker = () => {
-    const [language, setLanguage] = useState("english");
+    const [language, setLanguage] = useState("en");
+    const { t, i18n } = useTranslation();
+
+    const handleLanguageChange = (e: SelectChangeEvent) => {
+        console.log(e.target.value);
+
+        i18n.changeLanguage(e.target.value);
+        setLanguage(e.target.value);
+    };
 
     return (
         <FormControl
             sx={{
-                width: 150,
+                width: 100,
             }}
             variant="standard"
         >
             <InputLabel id="language-input">
-                <LanguageOutlinedIcon sx={{ verticalAlign: "top" }} /> Language
+                <LanguageOutlinedIcon sx={{ verticalAlign: "middle" }} /> Language
             </InputLabel>
-            <Select
-                labelId="language-input"
-                value={language}
-                onChange={(e: SelectChangeEvent) => setLanguage(e.target.value)}
-            >
-                <MenuItem value={"english"}>English</MenuItem>
+            <Select labelId="language-input" value={language} onChange={handleLanguageChange}>
+                <MenuItem value={"en"}>English</MenuItem>
                 <MenuItem value={"bari"}>Bari</MenuItem>
             </Select>
         </FormControl>

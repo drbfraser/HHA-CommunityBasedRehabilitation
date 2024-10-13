5ca55705522ad550b23c652f0dd19dcdb053909d
Stokely Diamantis <stokely_diamantis@sfu.ca>
2024-10-13 15:27:43 -0700

    fix: update ZoneList styles to mui5

--- web/src/pages/ZoneList/ZoneList.styles.ts
+++ web/src/pages/ZoneList/ZoneList.styles.ts
@@ -1,46 +1,82 @@
-import makeStyles from '@mui/styles/makeStyles';
+import { SxProps, Theme } from '@mui/material';
 import { mediaCompressedDataGrid } from "styles/DataGrid.styles";
 import { mediaMobile } from "theme.styles";
 
-// todo: finish migration to sx
-export const useStyles = makeStyles(
-    {
-        container: {
-            height: "calc(100vh - 175px)",
-            minHeight: 400,
-            padding: "5px 0px 25px 0px",
+// todo: does index: 1 insertion order matter when converting to sx?
+export const zoneListStyles: Record<string, SxProps<Theme>> = {
+    container: {
+        height: "calc(100vh - 175px)",
+        minHeight: 400,
+        padding: "5px 0px 25px 0px",
+        [mediaMobile]: {
+            height: "calc(100vh - 150px)",
+            paddingBottom: "50px",
         },
-        topContainer: {
-            justifyContent: "flex-end",
-            display: "flex",
-        },
-        dataGridWrapper: {
+        [mediaCompressedDataGrid]: {
+            paddingBottom: "71px",
+        }
+    },
+    topContainer: {
+        justifyContent: "flex-end",
+        display: "flex",
+        [mediaCompressedDataGrid]: {
+            flexGrow: 1,
+            justifyContent: "center",
+        }
+    },
+    dataGridWrapper: {
+        height: "100%",
+        width: "100%",
+        [mediaCompressedDataGrid]: {
             height: "100%",
             width: "100%",
-        },
-        icon: {
-            padding: "0px 15px 0px 0px",
-        },
-        // [mediaMobile]: {
-        //     container: {
-        //         height: "calc(100vh - 150px)",
-        //         paddingBottom: "50px",
-        //     },
-        // },
-        // [mediaCompressedDataGrid]: {
-        //     container: {
-        //         paddingBottom: "71px",
-        //     },
-        //     topContainer: {
-        //         flexGrow: 1,
-        //         justifyContent: "center",
-        //     },
-        //     dataGridWrapper: {
-        //         height: "100%",
-        //         width: "100%",
-        //         marginTop: 0,
-        //     },
-        // },
+            marginTop: 0,
+        }
     },
-    { index: 1 }
-);
+    icon: {
+        padding: "0px 15px 0px 0px",
+    },
+}
+
+// todo: remove unused
+// export const useStyles = makeStyles(
+//     {
+//         container: {
+//             height: "calc(100vh - 175px)",
+//             minHeight: 400,
+//             padding: "5px 0px 25px 0px",
+//         },
+//         topContainer: {
+//             justifyContent: "flex-end",
+//             display: "flex",
+//         },
+//         dataGridWrapper: {
+//             height: "100%",
+//             width: "100%",
+//         },
+//         icon: {
+//             padding: "0px 15px 0px 0px",
+//         },
+//         [mediaMobile]: {
+//             container: {
+//                 height: "calc(100vh - 150px)",
+//                 paddingBottom: "50px",
+//             },
+//         },
+//         [mediaCompressedDataGrid]: {
+//             container: {
+//                 paddingBottom: "71px",
+//             },
+//             topContainer: {
+//                 flexGrow: 1,
+//                 justifyContent: "center",
+//             },
+//             dataGridWrapper: {
+//                 height: "100%",
+//                 width: "100%",
+//                 marginTop: 0,
+//             },
+//         },
+//     },
+//     { index: 1 }
+// );
--- web/src/pages/ZoneList/ZoneList.tsx
+++ web/src/pages/ZoneList/ZoneList.tsx
@@ -1,4 +1,5 @@
-import { useStyles } from "./ZoneList.styles";
+import { zoneListStyles } from "./ZoneList.styles";
+import { dataGridStyles } from "styles/DataGrid.styles";
 import SearchBar from "components/SearchBar/SearchBar";
 
 import AddLocationIcon from "@mui/icons-material/AddLocation";
@@ -10,7 +11,6 @@ import {
     GridRenderCellParams,
 } from "@mui/x-data-grid";
 import { LinearProgress, IconButton, Typography, Box } from "@mui/material";
-import { mediaCompressedDataGrid, useDataGridStyles } from "styles/DataGrid.styles";
 import { useRef } from "react";
 import { useHistory } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -18,7 +18,7 @@ import reqeustZoneRows from "./reqeustZoneRows";
 import React from "react";
 import { Cancel } from "@mui/icons-material";
 import { SearchOption } from "./searchOptions";
-import { mediaMobile } from "theme.styles";
+
 // import { IRouteParams } from "@cbr/common/forms/Zone/zoneFields";
 const RenderText = (params: GridRenderCellParams) => {
     return <Typography variant={"body2"}>{String(params.value)}</Typography>; // todo: String() ok?
@@ -35,11 +35,9 @@ const RenderLoadingOverlay = () => {
 };
 
 const RenderNoRowsOverlay = () => {
-    const styles = useDataGridStyles();
-
     return (
-        <GridOverlay className={styles.noRows}>
-            <Cancel color="primary" className={styles.noRowsIcon} />
+        <GridOverlay sx={dataGridStyles.noRows}>
+            <Cancel color="primary" sx={dataGridStyles.noRowsIcon} />
             <Typography color="primary">No Zones Found</Typography>
         </GridOverlay>
     );
@@ -52,8 +50,6 @@ const ZoneList = () => {
     const [isZoneHidden, setZoneHidden] = useState<boolean>(false);
     const [filteredRows, setFilteredRows] = useState<GridRowsProp>([]); // todo: RowsProp -> GridRowsProp ok?
     const [serverRows, setServerRows] = useState<GridRowsProp>([]);
-    const styles = useStyles();
-    const dataGridStyle = useDataGridStyles();
     const history = useHistory();
     const onZoneAddClick = () => history.push("/zone/new");
     // const { zone_name } = useRouteMatch<IRouteParams>().params;
@@ -94,48 +90,16 @@ const ZoneList = () => {
     }, [searchValue, searchOption, serverRows]);
 
     return (
-        <Box
-            sx={{
-                height: "calc(100vh - 175px)",
-                minHeight: 400,
-                padding: "5px 0px 25px 0px",
-                [mediaMobile]: {
-                    height: "calc(100vh - 150px)",
-                    paddingBottom: "50px",
-                },
-                [mediaCompressedDataGrid]: {
-                    paddingBottom: "71px",
-                },
-            }}
-        >
-            <Box
-                sx={{
-                    justifyContent: "flex-end",
-                    display: "flex",
-                    [mediaCompressedDataGrid]: {
-                        flexGrow: 1,
-                        justifyContent: "center",
-                    },
-                }}
-            >
-                <IconButton onClick={onZoneAddClick} className={styles.icon} size="large">
+        <Box sx={zoneListStyles.container}>
+            <Box sx={zoneListStyles.topContainer}>
+                <IconButton onClick={onZoneAddClick} sx={zoneListStyles.icon} size="large">
                     <AddLocationIcon />
                 </IconButton>
                 <SearchBar value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
             </Box>
-            <Box
-                sx={{
-                    height: "100%",
-                    width: "100%",
-                    [mediaCompressedDataGrid]: {
-                        height: "100%",
-                        width: "100%",
-                        marginTop: 0,
-                    }
-                }}
-            >
+            <Box sx={zoneListStyles.dataGridWrapper}>
                 <DataGrid
-                    className={dataGridStyle.datagrid}
+                    sx={dataGridStyles.datagrid}
                     loading={loading}
                     components={{
                         LoadingOverlay: RenderLoadingOverlay,

26e5ba1020114e53fb8577fa1d2c0acf7bbd7b20
Harry Nguyen <hln5@sfu.ca>
2024-10-29 12:46:33 -0700

    get counts of duplicate values for client risks

--- scripts/dump_db_as_csv/script.py
+++ scripts/dump_db_as_csv/script.py
@@ -1,6 +1,10 @@
 import os
 import subprocess
+from pathlib import Path
 from dotenv import load_dotenv
+import pandas as pd
+
+OUTPUT_DIR = f"{os.path.dirname(__file__)}/csv"
 
 
 def generate_csv_files():
@@ -17,7 +21,6 @@ def generate_csv_files():
             exit(1)
 
     def output_db_tables_as_csv():
-        OUTPUT_DIR = f"{os.path.dirname(__file__)}/db_table_csv"
         POSTGRES_USER = os.getenv("POSTGRES_USER")
         DB_NAME = "cbr"
 
@@ -38,9 +41,38 @@ def generate_csv_files():
 
 
 def cleanup_csv_files():
-    pass
+    REMOVED_COLUMNS = ["id", "client_id_id", "visit_id_id",
+                       "created_at", "server_created_at", "timestamp"]
+
+    for file in Path(OUTPUT_DIR).glob("*.csv"):
+        df = pd.read_csv(file)
+        df = df.drop(REMOVED_COLUMNS, axis=1, errors="ignore")
+        # print(df)
+
+        df.to_csv(f"{OUTPUT_DIR}/cleaned_{file.name}")
+
+
+def get_counts():
+    for file in Path(OUTPUT_DIR).glob("*cleaned_*.csv"):
+        df = pd.read_csv(file)
+
+        if ("clientrisk" in file.name):
+            print(file.name)
+            df = df.groupby(["risk_type", "requirement"])
+            df = df.size().reset_index(name="count")
+            df = df.sort_values(by="count", ascending=False)
+            df = df.reset_index()
+            print(df)
+            pass
+        elif ("improvement" in file.name):
+            pass
+        elif ("outcome" in file.name):
+            pass
+
+        # df.to_csv(f"{OUTPUT_DIR}/cleaned_{file.name}")
 
 
 if __name__ == '__main__':
-    generate_csv_files()
-    cleanup_csv_files()
+    # generate_csv_files()
+    # cleanup_csv_files()
+    get_counts()

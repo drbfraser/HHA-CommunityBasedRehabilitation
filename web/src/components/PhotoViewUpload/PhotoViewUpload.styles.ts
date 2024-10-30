84dc6a90be31cbf78e47f9d9e0695c631e80709d
Stokely Diamantis <stokely_diamantis@sfu.ca>
2024-10-30 01:08:16 -0700

    fix: resolve PhotoView width at 960px breakpoint

--- web/src/components/PhotoViewUpload/PhotoViewUpload.styles.ts
+++ web/src/components/PhotoViewUpload/PhotoViewUpload.styles.ts
@@ -12,6 +12,9 @@ export const photoViewUploadStyles: Record<string, SxProps<Theme>> = {
         marginLeft: "auto",
         marginRight: "auto",
         cursor: "pointer",
+        "@media (max-width: 960px)": {
+            width: "13rem",
+        },
     },
     profileUploadHover: {
         "&:hover": {
@@ -46,66 +49,4 @@ export const photoViewUploadStyles: Record<string, SxProps<Theme>> = {
         bottom: 0,
         textAlign: "center",
     },
-    "@media (max-width: 960px)": {
-        profileImgContainer: {
-            width: "13rem",
-        },
-    },
 };
-
-// todosd: remove unused
-// export const useStyles = makeStyles(
-//     {
-//         profileImgContainer: {
-//             "& .MuiCardContent-root": {
-//                 padding: "16px",
-//             },
-//             position: "relative",
-//             display: "block",
-//             marginBottom: 15,
-//             overflow: "hidden",
-//             marginLeft: "auto",
-//             marginRight: "auto",
-//             cursor: "pointer",
-//         },
-//         profileUploadHover: {
-//             "&:hover": {
-//                 opacity: 0.5,
-//                 "& $uploadIcon": {
-//                     opacity: 1,
-//                 },
-//             },
-//         },
-//         profilePicture: {
-//             minWidth: "100%",
-//             height: "auto",
-//             width: "50%",
-//             objectFit: "cover",
-//             marginLeft: "auto",
-//             marginRight: "auto",
-//             display: "block",
-//         },
-//         pictureModal: {
-//             width: "65vw",
-//             maxWidth: "400px",
-//         },
-//         uploadIcon: {
-//             color: "white",
-//             display: "block",
-//             position: "absolute",
-//             zIndex: 500,
-//             left: 0,
-//             right: 0,
-//             opacity: 0,
-//             top: "50%",
-//             bottom: 0,
-//             textAlign: "center",
-//         },
-//         "@media (max-width: 960px)": {
-//             profileImgContainer: {
-//                 width: "13rem",
-//             },
-//         },
-//     },
-//     { index: 1 }
-// );

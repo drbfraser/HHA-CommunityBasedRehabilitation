7ff1f80424b7ae9c565ae037269d34fc5c4cbf08
Harry Nguyen <hln5@sfu.ca>
2024-10-31 12:33:49 -0700

    refactor(db dump): use subdirectories for raw and cleaned output csv files

--- scripts/dump_db_as_csv/script.py
+++ scripts/dump_db_as_csv/script.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 import pandas as pd
 
 OUTPUT_DIR = f"{os.path.dirname(__file__)}/csv"
+RAW_OUTPUT_DIR = f"{OUTPUT_DIR}/raw"
+CLEANED_OUTPUT_DIR = f"{OUTPUT_DIR}/cleaned"
 
 
 def generate_csv_files():
@@ -23,8 +25,8 @@ def generate_csv_files():
     def output_db_tables_as_csv():
         POSTGRES_USER = os.getenv("POSTGRES_USER")
         DB_NAME = "cbr"
+        os.makedirs(RAW_OUTPUT_DIR, exist_ok=True)
 
-        os.makedirs(OUTPUT_DIR, exist_ok=True)
         for table_name in ["cbr_api_clientrisk", "cbr_api_improvement", "cbr_api_outcome"]:
             print(f"Outputting CSV file for: {table_name}")
             command = [
@@ -32,7 +34,9 @@ def generate_csv_files():
                 "psql", "-U", POSTGRES_USER, "-d", DB_NAME,
                 "-c", f"\copy {table_name} TO STDOUT WITH (FORMAT csv, HEADER)"
             ]
-            with open(os.path.join(OUTPUT_DIR, f"{table_name}.csv"), 'w') as output_file:
+
+            output_path = os.path.join(RAW_OUTPUT_DIR, f"raw_{table_name}.csv")
+            with open(output_path, 'w') as output_file:
                 subprocess.run(command, stdout=output_file, check=True)
 
     load_environment_vars()
@@ -43,17 +47,16 @@ def generate_csv_files():
 def cleanup_csv_files():
     REMOVED_COLUMNS = ["id", "client_id_id", "visit_id_id",
                        "created_at", "server_created_at", "timestamp"]
+    os.makedirs(CLEANED_OUTPUT_DIR, exist_ok=True)
 
-    for file in Path(OUTPUT_DIR).glob("*.csv"):
+    for file in Path(RAW_OUTPUT_DIR).glob("*.csv"):
         df = pd.read_csv(file)
         df = df.drop(REMOVED_COLUMNS, axis=1, errors="ignore")
-        # print(df)
-
-        df.to_csv(f"{OUTPUT_DIR}/cleaned_{file.name}")
+        df.to_csv(f"{CLEANED_OUTPUT_DIR}/cleaned_{file.name}")
 
 
 def get_counts():
-    for file in Path(OUTPUT_DIR).glob("*cleaned_*.csv"):
+    for file in Path(CLEANED_OUTPUT_DIR).glob("*.csv"):
         df = pd.read_csv(file)
 
         if ("clientrisk" in file.name):
@@ -63,16 +66,18 @@ def get_counts():
             df = df.sort_values(by="count", ascending=False)
             df = df.reset_index()
             print(df)
-            pass
         elif ("improvement" in file.name):
             pass
         elif ("outcome" in file.name):
             pass
+        else:
+            print(f"Error: unrecognized file name: {file.name}")
 
         # df.to_csv(f"{OUTPUT_DIR}/cleaned_{file.name}")
 
 
 if __name__ == '__main__':
-    # generate_csv_files()
-    # cleanup_csv_files()
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    generate_csv_files()
+    cleanup_csv_files()
     get_counts()

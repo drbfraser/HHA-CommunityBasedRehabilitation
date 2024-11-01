024f32493ff5c1c820defef23f6abc6414d39b11
Harry Nguyen <hln5@sfu.ca>
2024-11-01 16:46:06 -0700

    feat(modal form): have it so that the underlying value of the form is a string

--- mobile/src/components/ModalForm/ModalForm.tsx
+++ mobile/src/components/ModalForm/ModalForm.tsx
@@ -1,31 +1,58 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useMemo, useState } from "react";
 import { Pressable } from "react-native";
-import { TextInput, Portal, Modal, Text, Divider } from "react-native-paper";
+import { TextInput, Portal, Modal, Text, Divider, HelperText } from "react-native-paper";
 import TextCheckBox from "../TextCheckBox/TextCheckBox";
 import useStyles from "./ModalForm.styles";
+import { shouldShowError } from "../../util/formikUtil";
+import { FormikProps } from "formik";
+import { clientFieldLabels } from "@cbr/common";
 
 interface IProps {
-    title: string;
+    field: string;
     checkboxLabels?: string[];
-    hasFreeFormText?: boolean;
+    hasFreeformText?: boolean;
+    formikProps: FormikProps<any>;
 }
 
-const ModalForm: FC<IProps> = ({ title, checkboxLabels = [], hasFreeFormText = false }) => {
+const ModalForm: FC<IProps> = ({
+    field,
+    formikProps,
+    checkboxLabels = [],
+    hasFreeformText = false,
+}) => {
     const styles = useStyles();
     const [visible, setVisible] = useState(false);
     const [checkedItems, setCheckedItems] = useState<{ [key: string]: boolean }>(
         checkboxLabels.reduce((acc, label) => ({ ...acc, [label]: false }), {}) ?? {}
     );
+    const [freeformText, setFreeformText] = useState("");
+    const [formValue, setFormValue] = useState("");
 
+    useEffect(() => {
+        const newValue = Object.entries(checkedItems)
+            .filter(([_label, isChecked]) => isChecked)
+            .map(([label, _isChecked]) => label)
+            .concat(freeformText)
+            .join(",\n");
+        setFormValue(newValue);
+    }, [checkedItems, freeformText]);
+
+    const onOpen = () => {
+        setVisible(true);
+    };
+    const onClose = () => {
+        setVisible(false);
+        formikProps.setFieldTouched(field);
+        formikProps.setFieldValue(field, formValue);
+    };
+
+    const label = clientFieldLabels[field];
+    const hasError = shouldShowError(formikProps, field);
     return (
         <>
             <Portal>
-                <Modal
-                    visible={visible}
-                    onDismiss={() => setVisible(false)}
-                    contentContainerStyle={styles.modal}
-                >
-                    <Text>{title}</Text>
+                <Modal visible={visible} onDismiss={onClose} contentContainerStyle={styles.modal}>
+                    <Text>{label}</Text>
                     <Divider />
                     {checkboxLabels.map((label, index) => (
                         <TextCheckBox
@@ -39,16 +66,32 @@ const ModalForm: FC<IProps> = ({ title, checkboxLabels = [], hasFreeFormText = f
                             }
                         />
                     ))}
-                    {hasFreeFormText && <TextInput mode="outlined" label="Other" />}
+                    {hasFreeformText && (
+                        <TextInput
+                            mode="outlined"
+                            label="Other"
+                            value={freeformText}
+                            onChangeText={(text) => setFreeformText(text)}
+                        />
+                    )}
                 </Modal>
             </Portal>
 
             <TextInput
                 editable={false}
                 mode="outlined"
-                label="show modal"
-                render={() => <Pressable onPress={() => setVisible(true)} style={styles.button} />}
+                label={`xxx${label}xxx`}
+                value={formValue}
+                error={hasError}
+                render={() => (
+                    <Pressable onPress={onOpen} style={styles.button}>
+                        <Text>{formValue}</Text>
+                    </Pressable>
+                )}
             />
+            <HelperText type="error" visible={hasError}>
+                {formikProps.errors[field] as string}
+            </HelperText>
         </>
     );
 };

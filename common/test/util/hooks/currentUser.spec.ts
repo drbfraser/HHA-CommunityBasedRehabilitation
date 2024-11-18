f24637c72be2b2a6cf12c4bbaa526f776979827c
Stokely Diamantis <stokely_diamantis@sfu.ca>
2024-11-18 00:08:52 -0800

    fix: make type explicit

--- common/test/util/hooks/currentUser.spec.ts
+++ common/test/util/hooks/currentUser.spec.ts
@@ -21,7 +21,7 @@ const resetFetchMocks = fetchMock.reset.bind(fetchMock);
 
 
 const testUser: IUser = {
-    id: 1,
+    id: "1",
     username: "user",
     first_name: "John",
     last_name: "",

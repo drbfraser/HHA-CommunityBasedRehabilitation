acb41635ea8dd71f03c061cf4eb2ed0a8e5482ac
Stokely Diamantis <stokely_diamantis@sfu.ca>
2024-11-18 19:12:14 -0800

    ci: print exentlet timer exceptions

--- server/cbr/wsgi.py
+++ server/cbr/wsgi.py
@@ -12,6 +12,7 @@ import eventlet  # concurrent networking library
 
 # replaces blocking function with async functions
 eventlet.monkey_patch(socket=True, select=True, thread=True)
+eventlet.debug.hub_exceptions(True)
 
 # import sys
 

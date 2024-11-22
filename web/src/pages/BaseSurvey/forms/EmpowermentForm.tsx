cd932ecaef05b5b7c0a89535070c4ca9a8646f9b
Harry Nguyen <hln5@sfu.ca>
2024-11-21 19:00:36 -0800

    refactor(baseline survey): remove br tags and extract components

---
+++ web/src/pages/BaseSurvey/forms/EmpowermentForm.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Field, FormikProps } from "formik";
+import { CheckboxWithLabel, TextField } from "formik-mui";
+import { FormControl } from "@mui/material";
+
+import {
+    baseFieldLabels,
+    BaseSurveyFormField,
+} from "@cbr/common/forms/BaseSurvey/baseSurveyFields";
+import { FieldIndent } from "../baseSurvey.style";
+
+const EmpowermentForm = ({ formikProps }: { formikProps: FormikProps<any> }) => {
+    return (
+        <FormControl fullWidth variant="outlined">
+            <Field
+                component={CheckboxWithLabel}
+                type="checkbox"
+                color="secondary"
+                name={BaseSurveyFormField.memOfOrgan}
+                Label={{ label: baseFieldLabels[BaseSurveyFormField.memOfOrgan] }}
+            />
+            {formikProps.values[BaseSurveyFormField.memOfOrgan] && (
+                <FieldIndent>
+                    <Field
+                        component={TextField}
+                        multiline
+                        variant="outlined"
+                        label={baseFieldLabels[BaseSurveyFormField.organization]}
+                        name={BaseSurveyFormField.organization}
+                        fullWidth
+                    />
+                </FieldIndent>
+            )}
+
+            <FormControl fullWidth variant="outlined">
+                <Field
+                    component={CheckboxWithLabel}
+                    type="checkbox"
+                    color="secondary"
+                    name={BaseSurveyFormField.awareRight}
+                    Label={{ label: baseFieldLabels[BaseSurveyFormField.awareRight] }}
+                />
+
+                <Field
+                    component={CheckboxWithLabel}
+                    type="checkbox"
+                    color="secondary"
+                    name={BaseSurveyFormField.ableInfluence}
+                    Label={{ label: baseFieldLabels[BaseSurveyFormField.ableInfluence] }}
+                />
+            </FormControl>
+        </FormControl>
+    );
+};
+
+export default EmpowermentForm;

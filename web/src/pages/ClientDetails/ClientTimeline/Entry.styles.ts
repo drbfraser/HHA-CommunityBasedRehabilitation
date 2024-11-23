18f7eba178b0bfeb200ced5f6da4a0f2ca2cffa5
Harry Nguyen <hln5@sfu.ca>
2024-11-23 15:37:09 -0800

    refactor(referral entry): rework styles

--- web/src/pages/ClientDetails/ClientTimeline/Entry.styles.ts
+++ web/src/pages/ClientDetails/ClientTimeline/Entry.styles.ts
@@ -1,5 +1,7 @@
 import { themeColors } from "@cbr/common/util/colors";
-import { SxProps, Theme } from "@mui/material";
+import { AccordionDetails, DialogContent, styled, SxProps, Theme } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import ScheduleIcon from "@mui/icons-material/Schedule";
 
 export const entryStyles: Record<string, SxProps<Theme>> = {
     impOutcomeAccordion: {
@@ -13,19 +15,33 @@ export const entryStyles: Record<string, SxProps<Theme>> = {
             padding: "8px 16px",
         },
     },
-    completeIcon: {
-        color: themeColors.riskGreen,
-        verticalAlign: "text-top",
-    },
-    pendingIcon: {
-        color: themeColors.riskRed,
-        verticalAlign: "text-top",
-    },
-    resolveAccordion: {
-        display: "block",
-        textAlign: "right",
-    },
-    resolveBtn: {
-        color: themeColors.riskGreen,
-    },
 };
+
+export const ReferralDialogContent = styled(DialogContent)({
+    display: "flex",
+    flexDirection: "column",
+    gap: "1em",
+});
+
+export const ReferralSummaryContainer = styled("div")({
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "0.5rem",
+});
+
+export const ResolveAccordion = styled(AccordionDetails)({
+    display: "block",
+    textAlign: "right",
+});
+
+export const CompleteIcon = styled(CheckCircleIcon)({
+    color: themeColors.riskGreen,
+    verticalAlign: "text-top",
+    marginRight: "5px",
+});
+
+export const PendingIcon = styled(ScheduleIcon)({
+    color: themeColors.riskRed,
+    verticalAlign: "text-top",
+    marginRight: "5px",
+});

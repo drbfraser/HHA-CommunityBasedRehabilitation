fc53da039522aefbcbcd12fe2af7580ff6954f98
Stokely Diamantis <stokely_diamantis@sfu.ca>
2024-12-13 05:17:38 -0800

    deps: update greenlet to 3.1.1

--- server/cbr/wsgi.py
+++ server/cbr/wsgi.py
@@ -22,21 +22,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cbr.settings")
 application = get_wsgi_application()
 application = socketio.WSGIApp(sio, application)
 
-# TEST
-import socket
-import select
-
-if eventlet.patcher.is_monkey_patched(socket):
-    print("socket is correctly monkey_patched")
-else:
-    print("socket is NOT correctly monkey_patched")
-
-if eventlet.patcher.is_monkey_patched(select):
-    print("select is correctly monkey_patched")
-else:
-    print("select is NOT correctly monkey_patched")
-# /TEST
-
 if DEBUG:
     # SocketIO requires a WSGI server to run. By default, when running in debug mode, there is no WSGI
     # server running. We don't want to run Gunicorn locally because it doesn't support hot reloading.

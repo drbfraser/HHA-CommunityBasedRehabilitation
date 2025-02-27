8af8f225de6150a382ca0e8ebb1a218e7b52a113
eunsongkoh <eunsongkohh@gmail.com>
2025-02-27 13:02:30 -0800

    wip switched viewing previous goals ui

--- web/src/pages/ClientDetails/PreviousGoals/PreviousGoalsModal.styles.ts
+++
@@ -1,7 +0,0 @@
-import { DialogContent, Paper } from "@mui/material";
-import { styled } from "@mui/system";
-
-export const StyledTablePaper = styled(Paper)({
-    height: 300,
-    width: "100%",
-});
--- web/src/pages/ClientDetails/PreviousGoals/PreviousGoalsModal.tsx
+++ web/src/pages/ClientDetails/PreviousGoals/PreviousGoalsModal.tsx
@@ -1,30 +1,30 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import React from "react";
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TablePagination,
+    TableRow,
+} from "@mui/material";
+import React, { useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { StyledTablePaper } from "./PreviousGoalsModal.styles";
 
 interface IModalProps {
     close: () => void;
 }
 const PreviousGoalsModal = (props: IModalProps) => {
-    const { t } = useTranslation();
-    const paginationModel = { page: 0, pageSize: 5 };
-    const columns: GridColDef[] = [
-        { field: "riskLevel", headerName: "Risk Level" },
-        { field: "area", headerName: "Area" },
-        { field: "goalDesc", headerName: "Goal Description" },
-        { field: "startDate", headerName: "Start Date" },
-        { field: "achDate", headerName: "Achieved Date" },
-        { field: "status", headerName: "" },
-    ];
-
     const sampleData = [
         {
             id: 1,
             riskLevel: "Health",
             area: "Bidi Bidi",
-            goalDesc: "Textbooks",
+            goalDesc: "Textbooks1",
             startDate: "01/01/2024",
             achDate: "05/05/2024",
             status: "check",
@@ -33,7 +33,7 @@ const PreviousGoalsModal = (props: IModalProps) => {
             id: 2,
             riskLevel: "Health",
             area: "Bidi Bidi",
-            goalDesc: "Textbooks",
+            goalDesc: "Textbooks2",
             startDate: "01/01/2024",
             achDate: "05/05/2024",
             status: "check",
@@ -42,7 +42,7 @@ const PreviousGoalsModal = (props: IModalProps) => {
             id: 3,
             riskLevel: "Health",
             area: "Bidi Bidi",
-            goalDesc: "Textbooks",
+            goalDesc: "Textbooks3",
             startDate: "01/01/2024",
             achDate: "05/05/2024",
             status: "check",
@@ -51,7 +51,7 @@ const PreviousGoalsModal = (props: IModalProps) => {
             id: 4,
             riskLevel: "Health",
             area: "Bidi Bidi",
-            goalDesc: "Textbooks",
+            goalDesc: "Textbooks4",
             startDate: "01/01/2024",
             achDate: "05/05/2024",
             status: "check",
@@ -60,7 +60,7 @@ const PreviousGoalsModal = (props: IModalProps) => {
             id: 5,
             riskLevel: "Health",
             area: "Bidi Bidi",
-            goalDesc: "Textbooks",
+            goalDesc: "Textbooks5",
             startDate: "01/01/2024",
             achDate: "05/05/2024",
             status: "check",
@@ -69,24 +69,76 @@ const PreviousGoalsModal = (props: IModalProps) => {
             id: 6,
             riskLevel: "Health",
             area: "Bidi Bidi",
-            goalDesc: "Textbooks",
+            goalDesc: "Textbooks6",
             startDate: "01/01/2024",
             achDate: "05/05/2024",
             status: "check",
         },
     ];
+    const { t } = useTranslation();
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    // TODO: To be used when we make the initial GET for the data, prevents it from having to be loaded again
+    const memoizedSampleData = useMemo(() => [...sampleData], []);
+
+    const visibleRows = useMemo(
+        () => [...memoizedSampleData].slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+        [page, rowsPerPage, memoizedSampleData]
+    );
 
     return (
         <Dialog fullWidth maxWidth="lg" open={true} aria-labelledby="form-dialog-title">
             <DialogTitle id="form-dialog-title">Viewing Previous Goals</DialogTitle>
             <DialogContent>
-                <StyledTablePaper>
-                    <DataGrid
-                        rows={sampleData}
-                        columns={columns}
-                        initialState={{ pagination: paginationModel }}
-                    />
-                </StyledTablePaper>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Risk Level</TableCell>
+                                <TableCell align="right">Area</TableCell>
+                                <TableCell align="right">Goal Description</TableCell>
+                                <TableCell align="right">Start Date</TableCell>
+                                <TableCell align="right">Achieved Date</TableCell>
+                                <TableCell align="right">Status</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {visibleRows.map((data, index) => {
+                                return (
+                                    <TableRow key={data.id}>
+                                        <TableCell component="th" scope="row">
+                                            {data.riskLevel}
+                                        </TableCell>
+                                        <TableCell align="right">{data.area}</TableCell>
+                                        <TableCell align="right">{data.goalDesc}</TableCell>
+                                        <TableCell align="right">{data.startDate}</TableCell>
+                                        <TableCell align="right">{data.achDate}</TableCell>
+                                        <TableCell align="right">{data.status}</TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[]}
+                    component="div"
+                    count={sampleData.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
             </DialogContent>
             <DialogActions>
                 <Button

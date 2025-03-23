5d8975adac4ed59c38596a63f724265ab7b9f14c
Stokely Diamantis <stokely_diamantis@sfu.ca>
2025-03-23 02:27:05 -0700

    remove testing version number

--- mobile/src/screens/DashBoard/Dashboard.tsx
+++ mobile/src/screens/DashBoard/Dashboard.tsx
@@ -143,7 +143,6 @@ const Dashboard = () => {
             <ScrollView>
                 <View style={styles.row}>
                     <Text style={styles.title}>{t("general.dashboard")}</Text>
-                    <Text style={styles.title}> 0.74</Text>
                 </View>
                 <View>
                     {unreadAlertsCount > 0 ? (

4a7367658aecf04b834a29dd52752e8d35f5a034
kmbui <mkb18@sfu.ca>
2025-07-01 13:36:35 +0700

    feat: add goal status to outcomefield

--- web/src/pages/NewVisit/components/OutcomeField.tsx
+++ web/src/pages/NewVisit/components/OutcomeField.tsx
@@ -1,55 +1,112 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { FormLabel, MenuItem, Typography } from "@mui/material";
+import { Box, FormLabel, Typography, Grid } from "@mui/material";
 import { Field, useFormikContext } from "formik";
 import { TextField } from "formik-mui";
-
 import {
     visitFieldLabels,
     VisitFormField,
     OutcomeFormField,
-    GoalStatus,
     getVisitGoalLabel,
     getVisitGoalStatusLabel,
 } from "@cbr/common/forms/newVisit/visitFormFields";
-import { IRisk, RiskType } from "@cbr/common/util/risks";
+import { getRiskGoalsTranslationKey, IRisk, RiskType } from "@cbr/common/util/risks";
+import EditNoteTwoToneIcon from "@mui/icons-material/EditNoteTwoTone";
+import UpdateGoalStatus from "/Users/buimaikhanh/Downloads/415-HHA-CBR/web/src/pages/ClientDetails/Risks/UpdateGoalStatus";
+import { RiskLevel } from "@cbr/common/util/risks";
+import { OutcomeGoalMet } from "@cbr/common/util/visits";
+import GoalStatusChip from "components/GoalStatusChip/GoalStatusChip";
+import Stack from "@mui/material/Stack";
 
-const OutcomeField = ({ visitType, risks }: { visitType: VisitFormField; risks: IRisk[] }) => {
+interface IModalProps {
+    risk: IRisk;
+    setRisk: (risk: IRisk) => void;
+    close: () => void;
+    newGoal: boolean;
+    visitType: VisitFormField;
+    risks: IRisk[];
+}
+
+const OutcomeField = (props : IModalProps) => {
     const { t } = useTranslation();
-    const fieldName = `${VisitFormField.outcomes}.${visitType}`;
+    const fieldName = `${VisitFormField.outcomes}.${props.visitType}`;
     const { setFieldValue } = useFormikContext<any>();
-    const matchingRisk = risks.find(
-        (risk) => risk.risk_type === (visitType as unknown as RiskType)
+    const matchingRisk = props.risks.find(
+        (risk) => risk.risk_type === (props.visitType as unknown as RiskType)
     );
+
+    const [openEditGoals, setOpenEditGoals] = useState<boolean>(false);
+    const handleEditGoalsClick = () => {
+        setOpenEditGoals((prevOpen: boolean) => !prevOpen);
+    };
+
+    const [editedRisk, setEditedRisk] = useState<IRisk>(() => {
+            if (props.newGoal) {
+                return {
+                    ...props.risk,
+                    goal_name: "",
+                    requirement: "",
+                    goal_status: OutcomeGoalMet.ONGOING,
+                    comments: RiskLevel.LOW,
+                    risk_level: RiskLevel.LOW,
+                };
+            }
+            return props.risk;
+        });
 
     useEffect(() => {
         if (matchingRisk?.goal_status) {
-            setFieldValue(`${fieldName}.${OutcomeFormField.goalStatus}`, matchingRisk.goal_status);
+            setFieldValue(`${fieldName}.goal_status`, matchingRisk.goal_status);
         }
     }, [matchingRisk, fieldName, setFieldValue]);
 
     return (
-        <div>
-            <FormLabel focused={false}>{getVisitGoalLabel(t, visitType)}</FormLabel>
+        <>
+        {openEditGoals && (
+                        <UpdateGoalStatus
+                            risk={editedRisk}
+                            setRisk={setEditedRisk}
+                            close={() => handleEditGoalsClick()}
+                            transKey={getRiskGoalsTranslationKey(props.risk.risk_type)}
+                        />
+                    )}
+        
+            <FormLabel focused={false}>{getVisitGoalLabel(t, props.visitType)}</FormLabel>
             <Typography variant={"body1"}>{matchingRisk?.goal_name}</Typography>
             <br />
 
-            <FormLabel focused={false}>{getVisitGoalStatusLabel(t, visitType)}</FormLabel>
+            <FormLabel focused={false}>{getVisitGoalStatusLabel(t, props.visitType)}</FormLabel>
             <br />
-            <Field
-                component={TextField}
-                select
-                required
-                variant="outlined"
-                name={`${fieldName}.${OutcomeFormField.goalStatus}`}
-            >
-                {Object.values(GoalStatus).map((status) => (
-                    <MenuItem key={status} value={status}>
-                        {visitFieldLabels[status]}
-                    </MenuItem>
-                ))}
-            </Field>
+            <Grid container direction="column" spacing={3}>
+                <Grid item>
+                    <Stack direction="row" spacing={1}>
+                        <Box
+                            onClick={handleEditGoalsClick}
+                                sx={{
+                                cursor: "pointer",
+                                display: "inline-flex",
+                            }}
+                        >
+                        <GoalStatusChip
+                            goalStatus={editedRisk.goal_status}
+                        />
+                        </Box>
+                        <Box
+                            onClick={handleEditGoalsClick}
+                            sx={{
+                                cursor: "pointer",
+                                display: "inline-flex",
+                            }}
+                        >
+                        <EditNoteTwoToneIcon
+                            color="action"
+                            fontSize="medium"
+                        />
+                        </Box>
+                    </Stack>
+                </Grid>
+            </Grid>                                              
             <br />
             <br />
             <div>
@@ -66,8 +123,8 @@ const OutcomeField = ({ visitType, risks }: { visitType: VisitFormField; risks: 
                 />
                 <br />
             </div>
-        </div>
-    );
+            </>
+);
 };
 
 export default OutcomeField;

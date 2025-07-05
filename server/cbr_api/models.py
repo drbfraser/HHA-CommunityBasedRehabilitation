90bf287636209c657434238f6ab40c8fc4a8de33
StarbzYT <chhokaradarsh@gmail.com>
2025-07-05 13:27:31 -0700

    642 update client model risk level default and seedclients script to include active goals

--- server/cbr_api/models.py
+++ server/cbr_api/models.py
@@ -119,7 +119,7 @@ class RiskLevel(models.TextChoices):
 
     @staticmethod
     def getField():
-        return models.CharField(max_length=2, choices=RiskLevel.choices, default="LO")
+        return models.CharField(max_length=2, choices=RiskLevel.choices, default=RiskLevel.NOT_ACTIVE)
 
 
 client_picture_upload_dir = "images/clients"

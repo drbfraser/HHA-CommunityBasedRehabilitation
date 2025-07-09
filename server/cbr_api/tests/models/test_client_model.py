ef0e11ad5bf9de869eeffc4553ca276ce1e56d64
StarbzYT <chhokaradarsh@gmail.com>
2025-07-09 16:44:24 -0700

    659 add test case for client creation

--- server/cbr_api/tests/models/test_client_model.py
+++ server/cbr_api/tests/models/test_client_model.py
@@ -1,7 +1,9 @@
-from server.cbr_api.models import UserCBR, Zone
+from django.test import TestCase
+from cbr_api.models import Client, Disability, UserCBR, Zone
+from cbr_api.tests.helpers import create_client
 
 
-class ClientModelTests:
+class ClientModelTests(TestCase):
     def setUp(self):
         self.zone = Zone.objects.create(zone_name="Test Zone")
         self.super_user = UserCBR.objects.create(
@@ -10,3 +12,22 @@ class ClientModelTests:
             is_superuser=True,
             zone=self.zone,
         )
+        self.disability = Disability.objects.create(disability_type="Amputee")
+
+    def test_client_creation_with_required_fields(self):
+        new_client = create_client(
+            self.super_user,
+            "John",
+            "Doe",
+            Client.Gender.MALE,
+            "604-555-1234",
+            self.zone,
+        )
+        self.assertIsNotNone(new_client.id)
+        self.assertEqual(new_client.first_name, "John")
+        self.assertEqual(new_client.last_name, "Doe")
+        self.assertEqual(new_client.gender, Client.Gender.MALE)
+        self.assertEqual(new_client.phone_number, "604-555-1234")
+        self.assertEqual(new_client.zone, self.zone)
+        self.assertTrue(new_client.is_active)
+        self.assertEqual(new_client.hcr_type, Client.HCRType.NOT_SET)

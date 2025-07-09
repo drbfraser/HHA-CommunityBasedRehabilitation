1a8898e487249282cba090b57d15925f92823a76
StarbzYT <chhokaradarsh@gmail.com>
2025-07-09 16:26:20 -0700

    659 create a new test suite for client model and a setup method for the test case methods

---
+++ server/cbr_api/tests/models/test_client_model.py
@@ -0,0 +1,12 @@
+from server.cbr_api.models import UserCBR, Zone
+
+
+class ClientModelTests:
+    def setUp(self):
+        self.zone = Zone.objects.create(zone_name="Test Zone")
+        self.super_user = UserCBR.objects.create(
+            username="root",
+            password="root",
+            is_superuser=True,
+            zone=self.zone,
+        )

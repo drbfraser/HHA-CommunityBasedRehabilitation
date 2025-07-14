2f8e8347a0fc4b3d2feba76bae96661c55775735
kmbui <mkb18@sfu.ca>
2025-07-14 17:20:17 +0700

    format: show current goal string

--- web/src/pages/ClientDetails/Risks/ClientRisks.tsx
+++ web/src/pages/ClientDetails/Risks/ClientRisks.tsx
@@ -79,9 +79,14 @@ const ClientRisks = ({ clientInfo, refreshClient }: IProps) => {
                                 </Typography>
                             </>
                         ) : (
+                            <>
+                            <Typography variant="subtitle2" component="h6">
+                                    Current Goal
+                            </Typography>
                             <Typography variant="body2" component="p">
                                 No current goal set
                             </Typography>
+                            </>
                         )}
                     </CardContent>
 

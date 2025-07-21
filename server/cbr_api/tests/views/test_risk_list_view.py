0751998ed87369ad86b9c1279c9bef35e74efca6
StarbzYT <chhokaradarsh@gmail.com>
2025-07-21 15:53:33 -0700

    672 add testcase for fetching risks given a client id parameter

--- server/cbr_api/tests/views/test_risk_list_view.py
+++ server/cbr_api/tests/views/test_risk_list_view.py
@@ -16,3 +16,19 @@ class RiskListViewTests(RiskViewsTestCase):
         risk_ids = [risk["id"] for risk in response.data]
         self.assertIn(str(self.risk1.id), risk_ids)
         self.assertIn(str(self.risk2.id), risk_ids)
+    
+    def test_get_risks_filtered_by_client_id(self):
+        url = reverse("risk-list")
+        valid_client_id_response = self.client_api.get(url, {"client_id": self.client.id})
+
+        self.assertEqual(valid_client_id_response.status_code, 200)
+        self.assertEqual(len(valid_client_id_response.data), 2)
+
+        for risk_data in valid_client_id_response.data:
+            self.assertEqual(risk_data["client_id"], str(self.client.id))
+
+        invalid_client_id_response = self.client_api.get(url, {"client_id": "invalid-id"})
+
+        self.assertEqual(invalid_client_id_response.status_code, 200)
+        self.assertEqual(len(invalid_client_id_response.data), 0)
+    

3576f5fd0b1c613b178dabc569f3d2f27a64aa7c
Alex Cai <aca277@sfu.ca>
2025-09-21 20:46:56 -0700

    feat: add confirmation dialog to back button

--- web/src/components/GoBackButton/GoBackButton.tsx
+++ web/src/components/GoBackButton/GoBackButton.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
 import { Button, SxProps, Theme } from "@mui/material";
 import ArrowBack from "@mui/icons-material/ArrowBack";
+import UnsavedChanges from "components/Dialogs/UnsavedChanges";
 
 interface IProps {
     sx?: SxProps<Theme> | undefined;
@@ -11,12 +12,36 @@ interface IProps {
 const GoBackButton = ({ sx }: IProps) => {
     const { t } = useTranslation();
     const history = useHistory();
+    const [dialogOpen, setDialogOpen] = useState(false);
+
+    const handleBack = () => {
+        setDialogOpen(true);
+    };
+
+    const handleConfirm = () => {
+        setDialogOpen(false);
+        history.goBack();
+    };
+
+    const handleCancel = () => {
+        setDialogOpen(false);
+    };
 
     return (
-        <Button sx={sx} onClick={history.goBack}>
-            <ArrowBack />
-            {t("general.goBack")}
-        </Button>
+        <>
+            <Button sx={sx} onClick={handleBack}>
+                <ArrowBack />
+                {t("general.goBack")}
+            </Button>
+            <UnsavedChanges
+                open={dialogOpen}
+                setOpen={setDialogOpen}
+                onSave={handleConfirm}
+                onCancel={handleCancel}
+                description={t("general.goBackWarning")}
+                saveBtnMsg={t("general.confirm")}
+            />
+        </>
     );
 };
 

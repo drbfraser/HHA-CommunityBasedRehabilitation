faa96d9ef18f996fce668fe2a7f7561d264575a0
StarbzYT <chhokaradarsh@gmail.com>
2025-10-20 19:02:05 -0700

    742 setup test suite for adminstatsfollowupvisits

--- server/cbr_api/tests/serializers/test_admin_stats_serializers.py
+++ server/cbr_api/tests/serializers/test_admin_stats_serializers.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from cbr_api.models import Zone
-from cbr_api.serializers import AdminStatsNewClientsSerializer
+from cbr_api.serializers import (
+    AdminStatsFollowUpVisitsSerializer,
+    AdminStatsNewClientsSerializer,
+)
 
 
 class AdminStatsNewClientsSerializerTests(TestCase):
@@ -42,3 +45,23 @@ class AdminStatsNewClientsSerializerTests(TestCase):
         self.assertEqual(validated["male_adult_total"], 18)
         self.assertEqual(validated["female_child_total"], 8)
         self.assertEqual(validated["male_child_total"], 9)
+
+
+class AdminStatsFollowUpVisitsSerializerTests(TestCase):
+    def setUp(self):
+        self.zone = Zone.objects.create(zone_name="Test Zone")
+
+    def test_serialization_with_valid_data(self):
+        data = {
+            "zone_id": self.zone.id,
+            "total": 120,
+            "hcr_type": "CBR",
+            "female_adult_total": 35,
+            "male_adult_total": 40,
+            "female_child_total": 20,
+            "male_child_total": 25,
+        }
+
+        serializer = AdminStatsFollowUpVisitsSerializer(data=data)
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+        self.assertEqual(serializer.validated_data["total"], 120)

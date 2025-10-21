dac818ff6020b93b9605421aaffef0f0e8e14547
StarbzYT <chhokaradarsh@gmail.com>
2025-10-20 18:50:23 -0700

    742 setup testcases for adminstats new clients serializer

--- server/cbr_api/tests/serializers/test_admin_stats_serializers.py
+++ server/cbr_api/tests/serializers/test_admin_stats_serializers.py
@@ -1,28 +1,44 @@
 from django.test import TestCase
 from cbr_api.models import Zone
-from cbr_api.serializers import ConfigStatsSerializer
+from cbr_api.serializers import AdminStatsNewClientsSerializer
 
 
-class ConfigStatsSerializerTests(TestCase):
-    """Tests for the dynamic ConfigStatsSerializer factory"""
-
+class AdminStatsNewClientsSerializerTests(TestCase):
     def setUp(self):
         self.zone = Zone.objects.create(zone_name="Test Zone")
 
-    def test_create_serializer_without_stat_types(self):
-        TestSerializer = ConfigStatsSerializer.create_serializer("TestSerializer")
-
+    def test_serialization_with_valid_data(self):
         data = {
             "zone_id": self.zone.id,
-            "total": 100,
+            "total": 50,
             "hcr_type": "CBR",
-            "female_adult_total": 25,
-            "male_adult_total": 30,
-            "female_child_total": 20,
-            "male_child_total": 25,
+            "female_adult_total": 15,
+            "male_adult_total": 18,
+            "female_child_total": 8,
+            "male_child_total": 9,
         }
 
-        serializer = TestSerializer(data=data)
+        serializer = AdminStatsNewClientsSerializer(data=data)
         self.assertTrue(serializer.is_valid(), serializer.errors)
-        self.assertEqual(serializer.validated_data["total"], 100)
-        self.assertEqual(serializer.validated_data["female_adult_total"], 25)
+        self.assertEqual(serializer.validated_data["total"], 50)
+
+    def test_deserialization_output(self):
+        data = {
+            "zone_id": self.zone.id,
+            "total": 50,
+            "hcr_type": "CBR",
+            "female_adult_total": 15,
+            "male_adult_total": 18,
+            "female_child_total": 8,
+            "male_child_total": 9,
+        }
+
+        serializer = AdminStatsNewClientsSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+
+        # Verify all demographics are present
+        validated = serializer.validated_data
+        self.assertEqual(validated["female_adult_total"], 15)
+        self.assertEqual(validated["male_adult_total"], 18)
+        self.assertEqual(validated["female_child_total"], 8)
+        self.assertEqual(validated["male_child_total"], 9)

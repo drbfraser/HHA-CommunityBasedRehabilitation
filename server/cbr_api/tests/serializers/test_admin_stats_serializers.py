73b45d735290c40b17a5251ca6def6e28108c208
StarbzYT <chhokaradarsh@gmail.com>
2025-10-25 11:29:35 -0700

    create adminstatsserializer class and complete data test case

--- server/cbr_api/tests/serializers/test_admin_stats_serializers.py
+++ server/cbr_api/tests/serializers/test_admin_stats_serializers.py
@@ -5,6 +5,7 @@ from cbr_api.serializers import (
     AdminStatsDischargedClientsSerializer,
     AdminStatsFollowUpVisitsSerializer,
     AdminStatsNewClientsSerializer,
+    AdminStatsSerializer,
 )
 
 
@@ -119,3 +120,62 @@ class AdminStatsDisabilitySerializerTests(TestCase):
         serializer = AdminStatsDisabilitySerializer(data=data)
         self.assertFalse(serializer.is_valid())
         self.assertIn("total", serializer.errors)
+
+
+class AdminStatsSerializerTests(TestCase):
+    def setUp(self):
+        self.zone = Zone.objects.create(zone_name="Test Zone")
+
+    def test_serialization_with_complete_data(self):
+        data = {
+            "disabilities": [
+                {"disability_id": 1, "total": 20},
+                {"disability_id": 2, "total": 15},
+            ],
+            "clients_with_disabilities": 35,
+            "visits": [{"zone_id": self.zone.id, "total": 100, "hcr_type": "CBR"}],
+            "referrals_resolved": [
+                {"zone_id": self.zone.id, "total": 50, "hcr_type": "CBR"}
+            ],
+            "referrals_unresolved": [
+                {"zone_id": self.zone.id, "total": 10, "hcr_type": "CBR"}
+            ],
+            "new_clients": [
+                {
+                    "zone_id": self.zone.id,
+                    "total": 30,
+                    "hcr_type": "CBR",
+                    "female_adult_total": 10,
+                    "male_adult_total": 10,
+                    "female_child_total": 5,
+                    "male_child_total": 5,
+                }
+            ],
+            "discharged_clients": [
+                {
+                    "zone_id": self.zone.id,
+                    "total": 20,
+                    "hcr_type": "CBR",
+                    "female_adult_total": 8,
+                    "male_adult_total": 6,
+                    "female_child_total": 3,
+                    "male_child_total": 3,
+                }
+            ],
+            "follow_up_visits": [
+                {
+                    "zone_id": self.zone.id,
+                    "total": 80,
+                    "hcr_type": "CBR",
+                    "female_adult_total": 25,
+                    "male_adult_total": 30,
+                    "female_child_total": 12,
+                    "male_child_total": 13,
+                }
+            ],
+        }
+
+        serializer = AdminStatsSerializer(data=data)
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+        self.assertEqual(serializer.validated_data["clients_with_disabilities"], 35)
+        self.assertEqual(len(serializer.validated_data["disabilities"]), 2)
